5c342e301233a107615749a7431f6621470d315b
Sebastien Pennec <sebastien@qos.ch>
2006-08-14 18:44:19 +0000

    improved a test

--- logback-classic/src/test/java/ch/qos/logback/classic/LoggerContextTest.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/LoggerContextTest.java
@@ -113,10 +113,16 @@ public class LoggerContextTest extends TestCase {
   public void testStatusWithUnconfiguredContext() {
   	Logger logger = lc.getLogger(LoggerContextTest.class);
   	
-  	for (int i = 0; i < 10; i++) {
+  	for (int i = 0; i < 3; i++) {
   		logger.debug("test");
   	}
 
+  	logger = lc.getLogger("x.y.z");
+  	
+  	for (int i = 0; i < 3; i++) {
+  		logger.debug("test");
+  	}
+  	
   	StatusManager sm = lc.getStatusManager();
   	assertTrue("StatusManager has recieved too many messages", sm.getCount() == 1);
   }
