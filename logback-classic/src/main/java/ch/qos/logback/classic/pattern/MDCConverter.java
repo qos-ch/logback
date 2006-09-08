e01f51e6d4c4fa0517da7eb0535cc4de5f322352
Ceki Gulcu <ceki@qos.ch>
2006-09-08 17:06:38 +0000

    slightly optimized version

--- logback-classic/src/main/java/ch/qos/logback/classic/pattern/MDCConverter.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/pattern/MDCConverter.java
@@ -5,20 +5,32 @@ import java.util.Map;
 import ch.qos.logback.classic.spi.LoggingEvent;
 
 public class MDCConverter extends ClassicConverter {
-	
-	public MDCConverter() {
-	}
 
-	@Override
-	public String convert(Object event) {
-		LoggingEvent loggingEvent = (LoggingEvent)event;
-		Map<String, String> mdcPropertyMap = loggingEvent.getMDCPropertyMap();
-		if(mdcPropertyMap != null) {
-			return loggingEvent.getMDCPropertyMap().get(getFirstOption());
-		} else {
-			return "";
-		}
-	}
+  String key;
+  
+  public MDCConverter() {
+  }
+
+  @Override
+  public void start() {
+    key = getFirstOption();
+    super.start();
+  }
+  
+  @Override
+  public void stop() {
+    key = null;
+    super.stop();
+  }
+  
+  @Override
+  public String convert(Object event) {
+    LoggingEvent loggingEvent = (LoggingEvent) event;
+    Map<String, String> mdcPropertyMap = loggingEvent.getMDCPropertyMap();
+    if (mdcPropertyMap != null) {
+      return loggingEvent.getMDCPropertyMap().get(key);
+    } else {
+      return "";
+    }
+  }
 }
-
-
