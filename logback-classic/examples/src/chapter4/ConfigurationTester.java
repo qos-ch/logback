0063e2e80e4295b2d38eb7036d62bae4147f8431
Sebastien Pennec <sebastien@qos.ch>
2006-09-25 15:01:50 +0000

    - added a chapter4 package in the examples, containing several configuration files for logback, as well as a ConfigurationTester class to launch logback with a specified config. file.

---
+++ logback-classic/examples/src/chapter4/ConfigurationTester.java
@@ -0,0 +1,43 @@
+package chapter4;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.MDC;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.util.StatusPrinter;
+import chapter4.sub.sample.Bar;
+/**
+ * 
+ * This class can be used to check the result of a configuration file.
+ * <p>
+ * When all the logback-core, logback-classic and their dependencies have been added
+ * to the ClassPath, one can launch this class using the following command:
+ * <p>
+ * java chapter4.ConfigurationTester chapter4/conf/name_of_the_configuration_file.xml
+ *
+ * @author S&eacute;bastien Pennec
+ */
+public class ConfigurationTester {
+
+  public static void main(String[] args) throws InterruptedException {
+    Logger logger = (Logger) LoggerFactory.getLogger(ConfigurationTester.class);
+    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+    JoranConfigurator configurator = new JoranConfigurator();
+    configurator.setContext(lc);
+    configurator.doConfigure(args[0]);
+
+    logger.debug("**Hello {}", new Bar());
+    MDC.put("testKey", "testValueFromMDC");
+    MDC.put("testKey2", "value2");
+    for (int i = 0; i < 10; i++) {
+      logger.debug("logging statement " + i);
+    }
+    Bar bar = new Bar();
+    bar.createLoggingRequest();
+
+    StatusPrinter.print(lc.getStatusManager());
+  }
+}
