af6fb3c9924213bf3270129a1787acd0a4bfe45a
Sebastien Pennec <sebastien@qos.ch>
2006-10-06 07:41:51 +0000

    bigger warning...

--- logback-access/src/main/java/ch/qos/logback/access/pattern/PostContentConverter.java
+++ logback-access/src/main/java/ch/qos/logback/access/pattern/PostContentConverter.java
@@ -4,10 +4,12 @@ import ch.qos.logback.access.spi.AccessEvent;
 
 /**
  * This class is tied to the <code>postContent</code> conversion pattern.
- * 
+ * <p>
  * It has been removed from the {@link ch.qos.logback.access.PatternLayout} since
  * it needs further testing before being used widely.
- *
+ * <p>
+ * <strong>Please handle this class with caution if you wish to use it anyway!</strong>
+ * 
  * @author Ceki G&uuml;lc&uuml;
  * @author S&eacute;bastien Pennec
  */
