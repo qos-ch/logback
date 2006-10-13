51c8da6c04642a1ffeb4ff0372cbb265fea71b80
Sebastien Pennec <sebastien@qos.ch>
2006-10-13 15:40:10 +0000

    On going work on chapter 5 - added HTMLLayout + examples with Evaluators to layouts.xml - added a faq.fml file with a first questions about jetty
    - organised imports in LoggerContext.java

--- logback-classic/examples/src/chapter5/CallerEvaluatorExample.java
+++ logback-classic/examples/src/chapter5/CallerEvaluatorExample.java
@@ -5,19 +5,16 @@ import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
-import ch.qos.logback.core.util.StatusPrinter;
 
-public class EventEvaluatorExample {
+public class CallerEvaluatorExample {
 
   public static void main(String[] args) throws InterruptedException {
-    Logger logger = (Logger) LoggerFactory.getLogger(EventEvaluatorExample.class);
+    Logger logger = (Logger) LoggerFactory.getLogger(CallerEvaluatorExample.class);
     LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
 
     JoranConfigurator configurator = new JoranConfigurator();
     configurator.setContext(lc);
     configurator.doConfigure(args[0]);
-
-    StatusPrinter.print(lc);
     
     for (int i = 0; i < 5; i++) {
       if (i == 3) {
@@ -26,7 +23,5 @@ public class EventEvaluatorExample {
         logger.debug("logging statement" + i);
       }
     }
-    
-    StatusPrinter.print(lc);
   }
 }
---
+++ logback-classic/examples/src/chapter5/ExceptionEvaluatorExample.java
@@ -0,0 +1,33 @@
+package chapter5;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.util.StatusPrinter;
+
+public class ExceptionEvaluatorExample {
+
+  public static void main(String[] args) throws InterruptedException {
+    Logger logger = (Logger) LoggerFactory.getLogger(ExceptionEvaluatorExample.class);
+    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+    JoranConfigurator configurator = new JoranConfigurator();
+    configurator.setContext(lc);
+    configurator.doConfigure(args[0]);
+    
+    for (int i = 0; i < 5; i++) {
+      if (i == 3) {
+        Marker ignoreMarker = MarkerFactory.getMarker("IGNORE");
+        logger.debug(ignoreMarker, "logging statement" + i, new Exception("test"));
+      } else {
+        logger.debug("logging statement" + i, new Exception("test"));
+      }
+    }
+    
+    StatusPrinter.print(lc);
+  }
+}
