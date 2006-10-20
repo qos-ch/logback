68620dd00fcc6640e0eab5381c3212e906c4b74e
Ceki Gulcu <ceki@qos.ch>
2006-10-20 15:26:31 +0000

    minor change

--- logback-core/src/test/java/ch/qos/logback/core/joran/TrivialcConfiguratorTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/joran/TrivialcConfiguratorTest.java
@@ -1,3 +1,13 @@
+/**
+ * Logback: the generic, reliable, fast and flexible logging framework for Java.
+ * 
+ * Copyright (C) 2000-2006, QOS.ch
+ * 
+ * This library is free software, you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation.
+ */
+
 package ch.qos.logback.core.joran;
 
 import java.util.HashMap;
