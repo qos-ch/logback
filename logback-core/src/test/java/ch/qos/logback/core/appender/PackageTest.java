9d664509e7f51c0356a5e72e9978161781cb0ba7
Ceki Gulcu <ceki@qos.ch>
2006-10-20 15:26:56 +0000

    minor

--- logback-core/src/test/java/ch/qos/logback/core/appender/PackageTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/appender/PackageTest.java
@@ -1,6 +1,14 @@
+/**
+ * Logback: the generic, reliable, fast and flexible logging framework for Java.
+ * 
+ * Copyright (C) 2000-2006, QOS.ch
+ * 
+ * This library is free software, you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation.
+ */
+
 package ch.qos.logback.core.appender;
-
-
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -8,7 +16,7 @@ import junit.framework.TestSuite;
 
 public class PackageTest extends TestCase {
 
-	public static Test suite() {
+  public static Test suite() {
     TestSuite suite = new TestSuite();
     suite.addTestSuite(DummyAppenderTest.class);
     suite.addTestSuite(ConsoleAppenderTest.class);
