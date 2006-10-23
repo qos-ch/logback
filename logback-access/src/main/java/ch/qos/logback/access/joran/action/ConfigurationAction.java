07cbab8b0cbbeb35d19d13cd4b2827dfc55728c2
Ceki Gulcu <ceki@qos.ch>
2006-10-23 17:02:39 +0000

    - renaming of ch.qos.logback.core.joran.spi.ExecutionContext as InterpretationContext

--- logback-access/src/main/java/ch/qos/logback/access/joran/action/ConfigurationAction.java
+++ logback-access/src/main/java/ch/qos/logback/access/joran/action/ConfigurationAction.java
@@ -13,7 +13,7 @@ package ch.qos.logback.access.joran.action;
 import org.xml.sax.Attributes;
 
 import ch.qos.logback.core.joran.action.Action;
-import ch.qos.logback.core.joran.spi.ExecutionContext;
+import ch.qos.logback.core.joran.spi.InterpretationContext;
 
 
 
@@ -21,14 +21,14 @@ public class ConfigurationAction extends Action {
   static final String INTERNAL_DEBUG_ATTR = "debug";
   boolean attachment = false;
 
-  public void begin(ExecutionContext ec, String name, Attributes attributes) {
+  public void begin(InterpretationContext ec, String name, Attributes attributes) {
     //String debugAttrib = attributes.getValue(INTERNAL_DEBUG_ATTR);
 
     // the context is appender attachable, so it is pushed on top of the stack
     ec.pushObject(getContext());
   }
 
-  public void end(ExecutionContext ec, String name) {
+  public void end(InterpretationContext ec, String name) {
     ec.popObject();
   }
 }
