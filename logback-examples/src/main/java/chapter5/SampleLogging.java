e7d01be52dfa5274c4a8b184352ff5388331d076
Ceki Gulcu <ceki@qos.ch>
2006-10-29 20:39:55 +0000

    Loggers should be of type org.slf4j not logback-classic.

--- logback-examples/src/main/java/chapter5/SampleLogging.java
+++ logback-examples/src/main/java/chapter5/SampleLogging.java
@@ -1,8 +1,8 @@
 package chapter5;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
@@ -11,8 +11,7 @@ import ch.qos.logback.core.util.StatusPrinter;
 public class SampleLogging {
 
   public static void main(String[] args) {
-    Logger logger = (Logger) LoggerFactory
-        .getLogger(SampleLogging.class);
+    Logger logger = LoggerFactory.getLogger(SampleLogging.class);
     LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
 
     try {
