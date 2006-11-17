5d4ff0f061903ad620253e83e9c6b6680754eeed
Sebastien Pennec <sebastien@qos.ch>
2006-11-17 08:57:44 +0000

    One can now specify the key to retrieve user information from the MDC

--- logback-classic/src/main/java/ch/qos/logback/classic/turbo/UserTurboFilter.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/turbo/UserTurboFilter.java
@@ -13,6 +13,10 @@ import ch.qos.logback.core.spi.FilterReply;
  * 
  * When the given user is identified by this TubroFilter, 
  * the reply is based on the OnMatch option.
+ * The information is taken from the MDC. By default, 
+ * the key used to retrieve the user information is "user".
+ * One can optionnally set the key that will be used to 
+ * access the user information in the MDC.
  * 
  * To allow output for a user, set the OnMatch option
  * to ACCEPT. To disable output for the given user, set
@@ -20,18 +24,20 @@ import ch.qos.logback.core.spi.FilterReply;
  * 
  * By default, values of the OnMatch and OnMisMatch
  * options are NEUTRAL.
+ * 
  *
  * @author Ceki G&uuml;lc&uuml;
  * @author S&eacute;bastien Pennec
  */
 public class UserTurboFilter extends TurboFilter {
 
+  String MDCKey = ClassicGlobal.USER_MDC_KEY;
   String user;
   
   @Override
   public FilterReply decide(Marker marker, Logger logger, Level level, String format, Object[] params, Throwable t) {
 
-    String user = MDC.get(ClassicGlobal.USER_MDC_KEY);
+    String user = MDC.get(MDCKey);
     if (this.user.equals(user)) {
       return onMatch;
     }
@@ -40,6 +46,10 @@ public class UserTurboFilter extends TurboFilter {
   
   public void setUser(String user) {
     this.user = user;
+  }
+  
+  public void setMDCKey(String MDCKey) {
+    this.MDCKey = MDCKey;
   }
 
 }
