54b68c0f1b851fdc27924be47faf54256b8fba5d
Sebastien Pennec <sebastien@qos.ch>
2006-11-30 14:26:19 +0000

    importing slf4j logger

--- logback-examples/src/main/java/chapter4/ConfigurationTester.java
+++ logback-examples/src/main/java/chapter4/ConfigurationTester.java
@@ -1,8 +1,9 @@
 package chapter4;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.Logger;
+
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.MDC;
 import ch.qos.logback.classic.joran.JoranConfigurator;
