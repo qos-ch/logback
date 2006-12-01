12ae67c2ade0f18671cfb1d1ddae42657c965801
Ceki Gulcu <ceki@qos.ch>
2006-12-01 20:04:22 +0000

    reverting changes to keep John happy

--- logback-classic/src/test/java/ch/qos/logback/classic/util/Constants.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/util/Constants.java
@@ -1,26 +1,19 @@
 package ch.qos.logback.classic.util;
 
-import java.io.File;
-import java.util.Properties;
 
 public class Constants {
 	
-  public static final String TEST_DIR_PREFIX;
+  public static final String TEST_DIR_PREFIX =  "src/test/";
   
-  static {
-    
-    Properties p = System.getProperties();
-    for(Object o: p.keySet()) {
-      System.out.println(o+"="+p.get(o));
-    }
-    File f = new File("");
-    String path = "";
-    String absolute = f.getAbsolutePath();
-    if (!absolute.endsWith("logback-classic")) {
-      path = "logback-classic/";
-    }
-    TEST_DIR_PREFIX = path + "src/test/";
-  }
+//  static {
+//    File f = new File("");
+//    String path = "";
+//    String absolute = f.getAbsolutePath();
+//    if (!absolute.endsWith("logback-classic")) {
+//      path = "logback-classic/";
+//    }
+//    TEST_DIR_PREFIX = path + "src/test/";
+//  }
 
 
 
--- logback-core/src/test/java/ch/qos/logback/core/util/Constants.java
+++ logback-core/src/test/java/ch/qos/logback/core/util/Constants.java
@@ -1,19 +1,18 @@
 package ch.qos.logback.core.util;
 
-import java.io.File;
 
 public class Constants {
   
-  static {
-    File f = new File("");
-    String path = "";
-    String absolute = f.getAbsolutePath();
-    if (!absolute.endsWith("logback-core")) {
-      path = "logback-core/";
-    }
-    TEST_DIR_PREFIX = path + "src/test/";
-  }
+//  static {
+//    File f = new File("");
+//    String path = "";
+//    String absolute = f.getAbsolutePath();
+//    if (!absolute.endsWith("logback-core")) {
+//      path = "logback-core/";
+//    }
+//    TEST_DIR_PREFIX = path + "src/test/";
+//  }
 
-  public static final String TEST_DIR_PREFIX;
+  public static final String TEST_DIR_PREFIX = "src/test/";
 
 }
