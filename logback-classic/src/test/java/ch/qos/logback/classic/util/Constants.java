890a2dfb03c4278446525562cf85aaedb35f9f38
Ceki Gulcu <ceki@qos.ch>
2006-12-01 18:55:09 +0000

    just testing

--- logback-classic/src/test/java/ch/qos/logback/classic/util/Constants.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/util/Constants.java
@@ -1,10 +1,18 @@
 package ch.qos.logback.classic.util;
 
 import java.io.File;
+import java.util.Properties;
 
 public class Constants {
 	
+  public static final String TEST_DIR_PREFIX;
+  
   static {
+    
+    Properties p = System.getProperties();
+    for(Object o: p.keySet()) {
+      System.out.println(o+"="+p.get(o));
+    }
     File f = new File("");
     String path = "";
     String absolute = f.getAbsolutePath();
@@ -14,6 +22,6 @@ public class Constants {
     TEST_DIR_PREFIX = path + "src/test/";
   }
 
-  public static final String TEST_DIR_PREFIX;
+
 
 }
