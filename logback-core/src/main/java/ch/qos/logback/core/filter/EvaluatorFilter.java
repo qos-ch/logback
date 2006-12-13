85a4e6d2664b33faad4cb71e152032e39339051d
Sebastien Pennec <sebastien@qos.ch>
2006-12-13 14:00:45 +0000

    Removed unecessary class and moved its content into EvaluatorFilter

--- logback-core/src/main/java/ch/qos/logback/core/filter/EvaluatorFilter.java
+++ logback-core/src/main/java/ch/qos/logback/core/filter/EvaluatorFilter.java
@@ -4,9 +4,47 @@ import ch.qos.logback.core.boolex.EvaluationException;
 import ch.qos.logback.core.boolex.EventEvaluator;
 import ch.qos.logback.core.spi.FilterReply;
 
-public class EvaluatorFilter extends AbstractEvalutatorFilter {
+/**
+ * The value of the {@link #onMatch} and {@link #onMismatch} attributes is set to 
+ * {@link Filter.NEUTRAL}, so that a badly configured evaluator filter doesn't 
+ * disturb the functionning of the chain.
+ * </p>
+ * <p>
+ * It is expected that one of the two attributes will have its value changed to
+ * {@link Filter.ACCEPT} or {@link Filter.DENY}. That way, it is possible to decide if 
+ * a given result must be returned after the evaluation either failed or succeeded.
+ * </p>
+ * 
+ * @author Ceki G&uuml;lc&uuml;
+ * @author S&eacute;bastien Pennec
+ */
+
+public class EvaluatorFilter extends Filter {
 
   EventEvaluator evaluator;
+  
+  protected FilterReply onMatch = FilterReply.NEUTRAL;
+  protected FilterReply onMismatch = FilterReply.NEUTRAL;
+
+  final public void setOnMatch(String action) {
+    if ("NEUTRAL".equals(action)) {
+      onMatch = FilterReply.NEUTRAL;
+    } else if ("ACCEPT".equals(action)) {
+      onMatch = FilterReply.ACCEPT;
+    } else if ("DENY".equals(action)) {
+      onMatch = FilterReply.DENY;
+    }
+  }
+
+  final public void setOnMismatch(String action) {
+    if ("NEUTRAL".equals(action)) {
+      onMismatch = FilterReply.NEUTRAL;
+    } else if ("ACCEPT".equals(action)) {
+      onMismatch = FilterReply.ACCEPT;
+    } else if ("DENY".equals(action)) {
+      onMismatch = FilterReply.DENY;
+    }
+  }
   
   @Override
   public void start() {
