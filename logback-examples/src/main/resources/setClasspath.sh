e8f2624854a9d20a75eafe6d86c9a46839ca4609
Ceki Gulcu <ceki@qos.ch>
2006-12-20 20:25:15 +0000

    On Unix the separator for variables is ':' not ';'

--- logback-examples/src/main/resources/setClasspath.sh
+++ logback-examples/src/main/resources/setClasspath.sh
@@ -4,12 +4,12 @@
 # This script will add logback jars to your classpath.
 ##
 
-LB_HOME=c:/SET/THIS/PARAMETER/TO/THE/FOLDER/WHERE/YOU/INSTALLED/LOGBACK
+LB_HOME=/SET/THIS/PARAMETER/TO/THE/DIRECTORY/WHERE/YOU/INSTALLED/LOGBACK
 
-CLASSPATH="${CLASSPATH};${LB_HOME}/logback-classic-${project.version}.jar"
-CLASSPATH="${CLASSPATH};${LB_HOME}/logback-core-${project.version}.jar"
-CLASSPATH="${CLASSPATH};${LB_HOME}/logback-examples/logback-examples-${project.version}.jar"
-CLASSPATH="${CLASSPATH};${LB_HOME}/logback-examples/lib/slf4j-api-${slf4j.version}.jar"
+CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-classic-${project.version}.jar"
+CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-core-${project.version}.jar"
+CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-examples/logback-examples-${project.version}.jar"
+CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-examples/lib/slf4j-api-${slf4j.version}.jar"
 
 export CLASSPATH
 
