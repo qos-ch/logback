1cbfcec3362619eb446fadbd99dfaf2fdd0ffb7f
Ceki Gulcu <ceki@qos.ch>
2007-01-16 15:02:14 +0000

    probably a more robust implementation

--- logback-access/src/main/java/ch/qos/logback/access/spi/Util.java
+++ logback-access/src/main/java/ch/qos/logback/access/spi/Util.java
@@ -1,20 +1,20 @@
 package ch.qos.logback.access.spi;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class Util {
-
+  static final int BUF_SIZE= 128;
+  
   public static String readToString(InputStream in) throws IOException {
-    StringBuffer sb = new StringBuffer();
-    BufferedReader inbr = new BufferedReader(new InputStreamReader(in));
-    String line;
-    while ((line = inbr.readLine()) != null) {
-      sb.append(line);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    byte[] buf = new byte[BUF_SIZE];
+    int n = 0;
+    while( (n = in.read(buf, 0, BUF_SIZE)) != -1) {
+      baos.write(buf, 0, n);
     }
-
-    return sb.toString();
+    
+    return baos.toString();
   }
 }
