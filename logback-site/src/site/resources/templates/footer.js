15cdb732de36ccb060741aa80c8d2ce09d0d5452
Ceki Gulcu <ceki@qos.ch>
2007-02-26 21:53:14 +0000

    doc updates

--- logback-site/src/site/resources/templates/footer.js
+++ logback-site/src/site/resources/templates/footer.js
@@ -1,4 +1,4 @@
 
 document.write('<p class="footer">')
-document.write('<a href="http://www.qos.ch/">QOS.ch</a>')
+document.write('Copyright &copy; 2000-2007  <a href="http://www.qos.ch/">QOS.ch</a>')
 document.write('</p>')
