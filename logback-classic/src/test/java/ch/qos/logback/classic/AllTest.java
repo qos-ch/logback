156457bf55d2e78e816b5b09c2b8c46edbe46dfc
Ceki Gulcu <ceki@qos.ch>
2007-02-27 10:51:47 +0000

    added missing PackageTest class

--- logback-classic/src/test/java/ch/qos/logback/classic/AllTest.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/AllTest.java
@@ -20,15 +20,18 @@ public class AllTest extends TestCase {
  
     suite.addTest(ch.qos.logback.classic.PackageTest.suite());
     suite.addTest(ch.qos.logback.classic.control.PackageTest.suite());
+    
     suite.addTest(ch.qos.logback.classic.joran.PackageTest.suite());
     suite.addTest(ch.qos.logback.classic.boolex.PackageTest.suite());
-
+    suite.addTest(ch.qos.logback.classic.selector.PackageTest.suite()); 
+    
     suite.addTest(ch.qos.logback.classic.html.PackageTest.suite()); 
     suite.addTest(ch.qos.logback.classic.net.PackageTest.suite());
     suite.addTest(ch.qos.logback.classic.pattern.PackageTest.suite()); 
     suite.addTest(ch.qos.logback.classic.db.PackageTest.suite()); 
     suite.addTest(ch.qos.logback.classic.spi.PackageTest.suite()); 
     suite.addTest(ch.qos.logback.classic.turbo.PackageTest.suite()); 
+    
     return suite;
   }
 }
