ebe21e265c4918aec382ad805a90362e62d84fea
Ceki Gulcu <ceki@qos.ch>
2007-03-06 19:11:05 +0000

    - added dependencies.html (a page explaining logback dependencies) - various other improvements to the docs

--- logback-site/src/site/pages/templates/right.js
+++ logback-site/src/site/pages/templates/right.js
@@ -1,6 +1,6 @@
 
 document.write('<p class="menu_header">New and noteworthy</p>')
-document.write('<p class="menu"><a href="10reasons.ppt">10 reasons for migrating</a>')
+document.write('<p class="menu"><a href="'+prefix+'10reasons.ppt">10 reasons for migrating</a>')
 
 document.write('<p class="menu"><a href="http://javablog.smilehouse.com/blog/default/Java/2007/02/02/Writing-rotated-and-compressed-access-logs-with-logback">Writing rotated and compressed access logs</a>')
 
