e781177f42f097593dac62cf463386e45352199a
Ceki Gulcu <ceki@qos.ch>
2007-03-19 19:08:32 +0000

    corrected URL

--- logback-site/src/site/pages/templates/header.js
+++ logback-site/src/site/pages/templates/header.js
@@ -1,6 +1,6 @@
 
 document.write('<p align="left">');
-document.write('<a href="http://logback.qos.ch.ch/">');
+document.write('<a href="http://logback.qos.ch/">');
 document.write('<img src="' + prefix + 'images/logos/lblogo.jpg" alt="" border="0"/>');
 document.write('</a>')
 document.write('</p>');
