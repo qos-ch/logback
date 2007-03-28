7b03c2369f9ad19833719ad87015a8551a2c21a3
Ceki Gulcu <ceki@qos.ch>
2007-03-28 19:44:29 +0000

    updated docs

--- logback-site/src/site/pages/templates/left.js
+++ logback-site/src/site/pages/templates/left.js
@@ -12,7 +12,7 @@ document.write('<p class="menu"><a href="http://logback.qos.ch/translator/">Log4
 document.write('<p class="menu"><a href="' + prefix + 'team.html">Logback Team</a></p>');
 document.write('<p><a class="x" href="http://www.qos.ch/"><img src="' + prefix + 'images/logos/qosLogo.png" /></a></p>');
 
-document.write('<span class="small">Last updated 2007-02-27</span>');
+document.write('<span class="small">Last updated 2007-03-29</span>');
 
 document.write('</p>');
 
