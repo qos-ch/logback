23892b3070254b7fa344066edfbc3dc6aaf0941c
Ceki Gulcu <ceki@qos.ch>
2007-03-30 19:57:38 +0000

    Added a link to Xavier Hanin's post

--- logback-site/src/site/pages/templates/right.js
+++ logback-site/src/site/pages/templates/right.js
@@ -2,6 +2,9 @@
 document.write('<p class="menu_header">New and noteworthy</p>')
 document.write('<p class="menu"><a href="'+prefix+'10reasons.ppt">10 reasons for migrating</a>')
 
+document.write('<p class="menu"><a href="http://xhab.blogspot.com/2007/03/new-logging-experience.html">A new logging experience!</a>')
+
+
 document.write('<p class="menu"><a href="http://javablog.smilehouse.com/blog/default/Java/2007/02/02/Writing-rotated-and-compressed-access-logs-with-logback">Writing rotated and compressed access logs</a>')
 
 
