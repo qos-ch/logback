b5c3bca8c73275ad40996cd5026f01a3113313a9
Ceki Gulcu <ceki@qos.ch>
2007-04-02 20:01:53 +0000

    minor fixes

--- logback-site/src/site/pages/templates/right.js
+++ logback-site/src/site/pages/templates/right.js
@@ -1,6 +1,7 @@
 
+//document.write('<p class="menu"><a href="'+prefix+'10reasons.ppt">10 reasons for migrating</a>')
+
 document.write('<p class="menu_header">New and noteworthy</p>')
-document.write('<p class="menu"><a href="'+prefix+'10reasons.ppt">10 reasons for migrating</a>')
 
 document.write('<p class="menu"><a href="http://xhab.blogspot.com/2007/03/new-logging-experience.html">A new logging experience!</a>')
 
