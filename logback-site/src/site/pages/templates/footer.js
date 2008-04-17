7b7e2f73491c7ce6897736aedda2bedf57f43c26
Ceki Gulcu <ceki@qos.ch>
2008-04-17 15:18:41 +0000

    - updating the error code for null_CS - copyright is 2000 to 2008 - other docs changes

--- logback-site/src/site/pages/templates/footer.js
+++ logback-site/src/site/pages/templates/footer.js
@@ -1,4 +1,4 @@
 
 document.write('<p class="footer">')
-document.write('Copyright &copy; 2000-2007  <a href="http://www.qos.ch/">QOS.ch</a>')
+document.write('Copyright &copy; 2000-2008  <a href="http://www.qos.ch/">QOS.ch</a>')
 document.write('</p>')
