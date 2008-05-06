4536560f34c32f11d632bdb609f095d71b4db98d
Ceki Gulcu <ceki@qos.ch>
2008-05-06 20:23:31 +0000

    - adding profiler examples

---
+++ logback-examples/src/main/java/chapter8/NestedProfilerExample.java
@@ -0,0 +1,46 @@
+package chapter8;
+
+import ch.qos.logback.classic.stopwatch.Profiler;
+
+public class NestedProfilerExample {
+  
+  public static void main(String[] args) {
+    Profiler profiler = new Profiler("BASIC");
+    profiler.start("A");
+    doA();
+       
+    profiler.start("B");
+    for (int i = 0; i < 5; i++) {
+      doB(i);
+    }
+    profiler.start("Other");
+    doOther();
+    profiler.stop().print();
+  }
+
+  public void subTask() {
+    
+  }
+  
+
+  static void doA() {
+    delay(10);
+  }
+  
+  static void doB(int millis) {
+    delay(millis);
+  }
+
+  static void doOther() {
+    delay(10);
+  }
+
+  
+  static  void delay(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+    }
+  }
+
+}
