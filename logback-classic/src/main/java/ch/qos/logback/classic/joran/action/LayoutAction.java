6ba9065ececc947ee4618b4e93ba648caf76234d
Ceki Gulcu <ceki@qos.ch>
2008-08-06 20:14:03 +0000

    - removing commented out code

--- logback-classic/src/main/java/ch/qos/logback/classic/joran/action/LayoutAction.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/joran/action/LayoutAction.java
@@ -5,9 +5,4 @@ import ch.qos.logback.core.joran.action.AbstractLayoutAction;
 
 
 public class LayoutAction extends AbstractLayoutAction<LoggingEvent> {
-
-//  protected boolean isOfCorrectType(Layout layout) {
-//    return (layout instanceof ClassicLayout);
-//  }
-
 }
