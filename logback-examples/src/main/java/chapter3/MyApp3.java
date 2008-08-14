f0874337b374367db5ba156827d2bcb45f4cdc36
Ceki Gulcu <ceki@qos.ch>
2008-08-14 16:01:22 +0000

    working on the docs

--- logback-examples/src/main/java/chapter3/MyApp3.java
+++ logback-examples/src/main/java/chapter3/MyApp3.java
@@ -1,7 +1,7 @@
 /**
  * Logback: the reliable, generic, fast and flexible logging framework.
  * 
- * Copyright (C) 1999-2006, QOS.ch
+ * Copyright (C) 1999-2008, QOS.ch
  * 
  * This library is free software, you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -10,22 +10,36 @@
 
 package chapter3;
 
+/**
+ * Demonstrates programmatic invocation of Joran.
+ * 
+ */
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
 import ch.qos.logback.core.util.StatusPrinter;
 
-public class MyApp2 {
-  final static Logger logger = LoggerFactory.getLogger(MyApp2.class);
+public class MyApp3 {
+  final static Logger logger = LoggerFactory.getLogger(MyApp3.class);
 
   public static void main(String[] args) {
-    logger.info("Entering application.");
-    
-    // print logback's internal status
+    // assume logback is in use
     LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-    StatusPrinter.print(lc);
-    
+
+    try {
+      JoranConfigurator configurator = new JoranConfigurator();
+      configurator.setContext(lc);
+      lc.shutdownAndReset();
+      configurator.doConfigure(args[0]);
+    } catch (JoranException je) {
+      StatusPrinter.print(lc);
+    }
+
+    logger.info("Entering application.");
+
     Foo foo = new Foo();
     foo.doIt();
     logger.info("Exiting application.");
