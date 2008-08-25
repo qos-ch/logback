c9db046758000d79ddf3b0c707f7eab0778ea9f6
Ceki Gulcu <ceki@qos.ch>
2008-08-25 19:54:06 +0000

    Related to LBSITE-16
    Documentation improvements many of which have been proposed by Anton Tagunov.

--- logback-examples/src/main/java/chapter4/ExitWoes1.java
+++ logback-examples/src/main/java/chapter4/ExitWoes1.java
@@ -1,7 +1,7 @@
 /**
- * Logback: the reliable, generic, fast and flexible logging framework.
+ * Logback: the generic, reliable, fast and flexible logging framework.
  * 
- * Copyright (C) 1999-2006, QOS.ch
+ * Copyright (C) 2000-2008, QOS.ch
  * 
  * This library is free software, you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -25,7 +25,7 @@ public class ExitWoes1 {
 
   public static void main(String[] args) throws Exception {
     LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-    lc.shutdownAndReset();//this is to cancel default-config.
+    lc.shutdownAndReset(); // we want to override the default-config.
     WriterAppender<LoggingEvent> writerAppender = new WriterAppender<LoggingEvent>();
     writerAppender.setContext(lc);
     writerAppender.setLayout(new EchoLayout<LoggingEvent>());
