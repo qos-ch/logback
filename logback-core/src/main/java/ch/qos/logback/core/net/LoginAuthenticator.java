5513a091a32455a77bb9559dcba98a5ca75236c9
Ceki Gulcu <ceki@qos.ch>
2008-10-15 19:27:54 +0000

    - copyright statement

--- logback-core/src/main/java/ch/qos/logback/core/net/LoginAuthenticator.java
+++ logback-core/src/main/java/ch/qos/logback/core/net/LoginAuthenticator.java
@@ -1,3 +1,13 @@
+/**
+ * Logback: the generic, reliable, fast and flexible logging framework.
+ * 
+ * Copyright (C) 2000-2008, QOS.ch
+ * 
+ * This library is free software, you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation.
+ */
+
 package ch.qos.logback.core.net;
 
 import javax.mail.Authenticator;
