452ba7c00b29ddba7d94544a8293e9386c9d8f0a
Ceki Gulcu <ceki@qos.ch>
2008-10-30 20:25:14 +0000

    - indentation & copyright notice

--- logback-classic/src/test/java/ch/qos/logback/classic/net/testObjectBuilders/LoggingEventBuilder.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/net/testObjectBuilders/LoggingEventBuilder.java
@@ -1,3 +1,12 @@
+/**
+ * Logback: the generic, reliable, fast and flexible logging framework.
+ * 
+ * Copyright (C) 2000-2008, QOS.ch
+ * 
+ * This library is free software, you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation.
+ */
 package ch.qos.logback.classic.net.testObjectBuilders;
 
 import ch.qos.logback.classic.Level;
@@ -7,15 +16,16 @@ import ch.qos.logback.classic.spi.LoggingEvent;
 
 public class LoggingEventBuilder implements Builder {
 
-	private Logger logger = new LoggerContext().getLogger(LoggerContext.ROOT_NAME);
-	
-	public Object build(int i) {
-		LoggingEvent le = new LoggingEvent();
-		le.setLevel(Level.DEBUG);
-		le.setLoggerRemoteView(logger.getLoggerRemoteView());
-		//le.setLogger(new LoggerContext().getLogger(LoggerContext.ROOT_NAME));
-		le.setMessage(MSG_PREFIX);
-		le.setThreadName("threadName");
-		return le;
-	}
+  private Logger logger = new LoggerContext()
+      .getLogger(LoggerContext.ROOT_NAME);
+
+  public Object build(int i) {
+    LoggingEvent le = new LoggingEvent();
+    le.setLevel(Level.DEBUG);
+    le.setLoggerRemoteView(logger.getLoggerRemoteView());
+    // le.setLogger(new LoggerContext().getLogger(LoggerContext.ROOT_NAME));
+    le.setMessage(MSG_PREFIX);
+    le.setThreadName("threadName");
+    return le;
+  }
 }
