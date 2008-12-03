f66e06329cf3e4c05ed7a36c8c1e01470e241afe
Ceki Gulcu <ceki@qos.ch>
2008-12-03 14:52:36 +0000

    Added test case related to LBCORE-67 as provided by Joern

--- logback-core/src/test/java/ch/qos/logback/core/spi/PackageTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/spi/PackageTest.java
@@ -9,16 +9,12 @@
  */
 package ch.qos.logback.core.spi;
 
-import junit.framework.JUnit4TestAdapter;
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AppenderAttachableImplTest.class ,AppenderAttachableImplLockTest.class})
 
 public class PackageTest {
-  public static Test suite() {
-    TestSuite suite = new TestSuite();
-    suite.addTest(new JUnit4TestAdapter(AppenderAttachableImplTest.class));
-    return suite;
-  }
-
 }
