c668c687310c1cf2e63046aa70339862c61418bd
Ceki Gulcu <ceki@qos.ch>
2009-01-25 18:33:24 +0000

    - rearranging the chapter on Joran (it is now part of the manual)

--- logback-site/src/site/pages/manual/menu.js
+++ logback-site/src/site/pages/manual/menu.js
@@ -2,10 +2,11 @@
 document.write('<p class="menu_header">Chapter Index</p>')
 document.write('<p class="menu"><a href="introduction.html"><b>Ch1: Introduction to logback</b></a></p>');
 document.write('<p class="menu"><a href="architecture.html"><b>Ch2: Architecture</b></a>');
-document.write('<p class="menu"><a href="joran.html"><b>Ch3: Logback configuration with Joran</b></a>');
+document.write('<p class="menu"><a href="configuration.html"><b>Ch3: Configuration</b></a>');
 document.write('<p class="menu"><a href="appenders.html"><b>Ch4: Appenders</b></a>');
 document.write('<p class="menu"><a href="layouts.html"><b>Ch5: Layouts</b></a>');
 document.write('<p class="menu"><a href="filters.html"><b>Ch6: Filter chains</b></a>');
 document.write('<p class="menu"><a href="mdc.html"><b>Ch7: Mapped Diagnostic Contexts</b></a>');
 document.write('<p class="menu"><a href="loggingSeparation.html"><b>Ch8: Logging Separation</b></a>');
 document.write('<p class="menu"><a href="jmxConfig.html"><b>Ch9: JMX Configurator</b></a>');
+document.write('<p class="menu"><a href="onJoran.html"><b>Ch9: Joran</b></a>');
