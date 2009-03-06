4c943060830039dd3b128f988a0c30e4d66500b2
Ceki Gulcu <ceki@qos.ch>
2009-03-06 16:54:46 +0000

    more javadocs + copyright statement

--- logback-classic/src/test/java/ch/qos/logback/classic/corpus/LogStatement.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/corpus/LogStatement.java
@@ -1,8 +1,25 @@
+/**
+ * Logback: the generic, reliable, fast and flexible logging framework.
+ * 
+ * Copyright (C) 2000-2009, QOS.ch
+ * 
+ * This library is free software, you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation.
+ */
 package ch.qos.logback.classic.corpus;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.IThrowableProxy;
 
+/**
+ * Captures the data contained within a log statement, that is the data that the
+ * developer puts in the source code when he writes:
+ * 
+ * <p>logger.debug("hello world");
+ * 
+ * @author Ceki G&uuml;lc&uuml; 
+ */
 public class LogStatement {
 
   final String loggerName;
