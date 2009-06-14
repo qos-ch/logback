04e8434c6f749d7c7ef95f18b9545b8740a97d6e
Ceki Gulcu <ceki@qos.ch>
2009-06-14 19:22:00 +0000

    LBCLASSIC-36 Test performance of Yoda time compared to SimpleDateFormat.
    Here are sample results on my machine:
    
    Raw Joda:               average tick 2714.052123 nanoseconds
    Sync SimpleDateFormat:  average tick 2129.857639 nanoseconds
    Raw SimpleDateFormat:   average tick 2138.306659 nanoseconds
    
    Conclusion: Yoda time and SimpleDataFormat have roughly equivalent
    performace, with the latter having the edge.

---
+++ logback-classic/src/test/java/ch/qos/logback/classic/issue/lbclassic36/TestDateFormat.java
@@ -0,0 +1,63 @@
+package ch.qos.logback.classic.issue.lbclassic36;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class TestDateFormat {
+  public static final String ISO8601_PATTERN = "yyyy-MM-dd HH:mm:ss,SSS";
+  static final long NANOS_IN_ONE_SEC = 1000 * 1000 * 1000L;
+
+  static long RUN_LENGTH = 1000*1000;
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      doRawJoda();
+      doRawSimpleDateFormat();
+      doSyncronizedSimpleDateFormat();
+    }
+
+    print("Raw Joda:              ", doRawJoda());
+    print("Sync SimpleDateFormat: ", doSyncronizedSimpleDateFormat());
+    print("Raw SimpleDateFormat:  ", doRawSimpleDateFormat());
+  }
+
+  static void print(String msg, double avg) {
+    System.out.println(msg + " average tick " + avg + " nanoseconds");
+  }
+
+  static double doRawJoda() {
+    DateTimeFormatter jodaFormat = DateTimeFormat.forPattern(ISO8601_PATTERN);
+    long timeInMillis = new Date().getTime();
+    long start = System.nanoTime();
+    for (int i = 0; i < RUN_LENGTH; ++i) {
+      jodaFormat.print(timeInMillis);
+    }
+    return (System.nanoTime() - start) * 1.0d / RUN_LENGTH;
+  }
+
+  static double doRawSimpleDateFormat() {
+    SimpleDateFormat simpleFormat = new SimpleDateFormat(ISO8601_PATTERN);
+    long timeInMillis = new Date().getTime();
+    long start = System.nanoTime();
+    for (int i = 0; i < RUN_LENGTH; ++i) {
+      simpleFormat.format(timeInMillis);
+    }
+    return (System.nanoTime() - start) * 1.0d / RUN_LENGTH;
+  }
+
+  static double doSyncronizedSimpleDateFormat() {
+    SimpleDateFormat simpleFormat = new SimpleDateFormat(ISO8601_PATTERN);
+    long timeInMillis = new Date().getTime();
+    long start = System.nanoTime();
+    for (int i = 0; i < RUN_LENGTH; ++i) {
+      synchronized (simpleFormat) {
+        simpleFormat.format(timeInMillis);
+      }
+    }
+    return (System.nanoTime() - start) * 1.0d / RUN_LENGTH;
+  }
+
+}
