4a1d2dfdc666f6821fd19ebfbb9021af4833b6b2
Ceki Gulcu <ceki@qos.ch>
2009-07-09 14:58:27 +0000

    Fix LBCORE-107

--- logback-core/src/main/java/ch/qos/logback/core/joran/JoranConfiguratorBase.java
+++ logback-core/src/main/java/ch/qos/logback/core/joran/JoranConfiguratorBase.java
@@ -1,7 +1,7 @@
 /**
- * Logback: the generic, reliable, fast and flexible logging framework for Java.
+ * Logback: the generic, reliable, fast and flexible logging framework.
  * 
- * Copyright (C) 2000-2007, QOS.ch
+ * Copyright (C) 2000-2009, QOS.ch
  * 
  * This library is free software, you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -85,7 +85,7 @@ abstract public class JoranConfiguratorBase extends GenericConfigurator {
     interpreter.addImplicitAction(nestedIA);
 
     NestedBasicPropertyIA nestedSimpleIA = new NestedBasicPropertyIA();
-    nestedIA.setContext(context);
+    nestedSimpleIA.setContext(context);
     interpreter.addImplicitAction(nestedSimpleIA);
   }
 
