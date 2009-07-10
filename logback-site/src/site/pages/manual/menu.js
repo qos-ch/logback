a69f5826707212815ddedc66cb4156b62b2a4a06
Ceki Gulcu <ceki@qos.ch>
2009-07-10 09:28:54 +0000

    - Filter chains chapters renamed as "Filters" as filters no longer maintain an internal   linked list structure. - Fixed LBSITE-30

--- logback-site/src/site/pages/manual/menu.js
+++ logback-site/src/site/pages/manual/menu.js
@@ -5,7 +5,7 @@ document.write('<p class="menu"><a href="architecture.html"><b>Ch2: Architecture
 document.write('<p class="menu"><a href="configuration.html"><b>Ch3: Configuration</b></a>');
 document.write('<p class="menu"><a href="appenders.html"><b>Ch4: Appenders</b></a>');
 document.write('<p class="menu"><a href="layouts.html"><b>Ch5: Layouts</b></a>');
-document.write('<p class="menu"><a href="filters.html"><b>Ch6: Filter chains</b></a>');
+document.write('<p class="menu"><a href="filters.html"><b>Ch6: Filters</b></a>');
 document.write('<p class="menu"><a href="mdc.html"><b>Ch7: Mapped Diagnostic Contexts</b></a>');
 document.write('<p class="menu"><a href="loggingSeparation.html"><b>Ch8: Logging Separation</b></a>');
 document.write('<p class="menu"><a href="jmxConfig.html"><b>Ch9: JMX Configurator</b></a>');
