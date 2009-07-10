96c7534d18a5461bf13648e941187e9b647997c1
Ceki Gulcu <ceki@qos.ch>
2009-07-10 09:38:46 +0000

    - linking to the W3 validator slows down the page

--- logback-site/src/site/pages/templates/footer.js
+++ logback-site/src/site/pages/templates/footer.js
@@ -4,12 +4,13 @@ document.write('<table class="footer">')
 document.write('<tr>')
 
 document.write('  <td>')
-document.write('    <a href="http://validator.w3.org/check?uri=referer">')
-document.write('      <img align="left" src="http://www.w3.org/Icons/valid-xhtml10"') 
-document.write('           alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>')
+document.write('  &nbsp;')
+//document.write('    <a href="http://validator.w3.org/check?uri=referer">')
+//document.write('      <img align="left" src="http://www.w3.org/Icons/valid-xhtml10"') 
+//document.write('           alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>')
 document.write('   </td>')
 
-document.write('<td valign="top">Copyright &copy; 2000-2008  <a href="http://www.qos.ch/">QOS.ch</a></td>')
+document.write('<td valign="top">Copyright &copy; 2000-2009  <a href="http://www.qos.ch/">QOS.ch</a></td>')
 
 document.write('</tr>')
 document.write('</table>')
