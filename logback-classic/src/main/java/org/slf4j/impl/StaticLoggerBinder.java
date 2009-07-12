f5ac0bb99edc6082bec7be8131e2e1dfdca8624b
Ceki Gulcu <ceki@qos.ch>
2009-07-12 19:18:30 +0000

    - request SLF4J 1.5.8

--- logback-classic/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ logback-classic/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -43,7 +43,7 @@ public class StaticLoggerBinder implements LoggerFactoryBinder {
    * against. The value of this field is usually modified with each release.
    */
   // to avoid constant folding by the compiler, this field must *not* be final
-  public static String REQUESTED_API_VERSION = "1.5.6"; // !final
+  public static String REQUESTED_API_VERSION = "1.5.8"; // !final
 
   final static String NULL_CS_URL = CoreConstants.CODES_URL + "#null_CS";
 
