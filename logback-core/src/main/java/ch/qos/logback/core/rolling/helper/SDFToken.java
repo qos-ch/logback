74d2526e2f1ad23f14dc653fc59578eafae18658
Ceki Gulcu <ceki@qos.ch>
2009-08-03 13:28:59 +0000

    ongoing work

--- logback-core/src/main/java/ch/qos/logback/core/rolling/helper/SDFToken.java
+++ logback-core/src/main/java/ch/qos/logback/core/rolling/helper/SDFToken.java
@@ -1,5 +1,11 @@
 /**
+ * Logback: the generic, reliable, fast and flexible logging framework.
  * 
+ * Copyright (C) 2000-2009, QOS.ch
+ * 
+ * This library is free software, you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation.
  */
 package ch.qos.logback.core.rolling.helper;
 
@@ -14,5 +20,12 @@ class SDFToken {
 
   void inc() {
     occurrences++;
+  }
+  
+  String toRegex() {
+    switch(c) {
+    case 'G':
+      return null;
+    }
   }
 }
