2fa846f25248559a413214c5ef2188af2ff7fe45
Ceki Gulcu <ceki@qos.ch>
2010-03-10 15:49:27 +0100

    - added a link to the article on making contributions.

--- logback-site/src/site/pages/templates/footer.js
+++ logback-site/src/site/pages/templates/footer.js
@@ -21,7 +21,7 @@ AAT = '@'
 DOOTT = '.'
 document.write('<tr>') 
 document.write('<td align="left" colspan="2">') 
-document.write('We are actively looking for volunteers to proofread the documentation. Please send your corrections or suggestions for improvement to "corrections' + AAT +'qos'+DOOTT+'ch".');
+document.write('We are actively looking for volunteers to proofread the documentation. Please send your corrections or suggestions for improvement to "corrections' + AAT +'qos'+DOOTT+'ch". See also the <a href="http://articles.qos.ch/contributing.html">instructions for contributors</a>.');
 document.write('</td>') 
 
 document.write('</table>')
