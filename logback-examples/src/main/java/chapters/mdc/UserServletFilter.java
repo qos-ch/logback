e13257d75b2b4439c4c98b85e25a76611080a3d4
Ceki Gulcu <ceki@qos.ch>
2010-03-18 19:08:24 +0100

    Fixed http://jira.qos.ch/browse/LBSITE-36

--- logback-examples/src/main/java/chapters/mdc/UserServletFilter.java
+++ logback-examples/src/main/java/chapters/mdc/UserServletFilter.java
@@ -23,38 +23,21 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.MDC;
 
 /**
  * A simple servlet filter that puts the username
- * found either in the Principle or as a session attribute
- * in the MDC.
+ * found either in the Principal.
  * 
- * The value is removed from the MDC once the request has been
+ * <p> The value is removed from the MDC once the request has been
  * fully processed.
- *
- * To be used, add the following lines to a web.xml file
- * 
- * <filter>
- *   <filter-name>User Servlet Filter</filter-name>
- *   <filter-class>
- *     chapters.mdc.UserServletFilter
- *   </filter-class>
- * </filter>
- * <filter-mapping>
- *   <filter-name>User Servlet Filter</filter-name>
- *   <url-pattern>/*</url-pattern>
- * </filter-mapping>
  *
  * @author S&eacute;bastien Pennec
  */
 public class UserServletFilter implements Filter {
 
-  boolean userRegistered = false;
-  
-  private final String userKey = "username";
+  private final String USER_KEY = "username";
   
   public void destroy() {
   }
@@ -62,25 +45,22 @@ public class UserServletFilter implements Filter {
   public void doFilter(ServletRequest request, ServletResponse response,
       FilterChain chain) throws IOException, ServletException {
 
+    boolean successfulRegistration = false;
     HttpServletRequest req = (HttpServletRequest) request;
     Principal principal = req.getUserPrincipal();
-    // Please note that we could have also used a cookie to 
-    // retreive the user name
+    // Please note that we also could have used a cookie to 
+    // retrieve the user name
     
     if (principal != null) {
       String username = principal.getName();
-      registerUsername(username);
-    } else {
-      HttpSession session = req.getSession();
-      String username = (String)session.getAttribute(userKey);
-      registerUsername(username);
+      successfulRegistration = registerUsername(username);
     }
     
     try {
       chain.doFilter(request, response);
     } finally {
-      if (userRegistered) {
-        MDC.remove(userKey);
+      if (successfulRegistration) {
+        MDC.remove(USER_KEY);
       }
     }
   }
@@ -88,11 +68,18 @@ public class UserServletFilter implements Filter {
   public void init(FilterConfig arg0) throws ServletException {
   }
   
-  private void registerUsername(String username) {
+  /**
+   * Register the user in the MDC under USER_KEY.
+   * 
+   * @param username
+   * @return true id the user can be successfully registered
+   */
+  private boolean registerUsername(String username) {
     if (username != null && username.trim().length() > 0) {
-      MDC.put(userKey, username);
-      userRegistered = true;
+      MDC.put(USER_KEY, username);
+      return true;
     }
+    return false;
   }
 
 }
