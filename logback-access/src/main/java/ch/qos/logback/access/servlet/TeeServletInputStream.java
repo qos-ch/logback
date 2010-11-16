8c9b86fb1553924df8ea682ca39ef336db2725ae
Ceki Gulcu <ceki@qos.ch>
2010-11-16 18:24:54 +0100

    - pom.xml clean up - Fix LBACCESS-10 - TeeServletInputStreamm barfs on input over 8K

--- logback-access/src/main/java/ch/qos/logback/access/servlet/TeeServletInputStream.java
+++ logback-access/src/main/java/ch/qos/logback/access/servlet/TeeServletInputStream.java
@@ -14,45 +14,66 @@
 package ch.qos.logback.access.servlet;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 
 class TeeServletInputStream extends ServletInputStream {
 
-  InputStream in;
-  byte[] inputBuffer;
+    InputStream in;
+    byte[] inputBuffer;
 
-  TeeServletInputStream(HttpServletRequest request) {
-    duplicateInputStream(request);
-  }
-
-  @Override
-  public int read() throws IOException {
-    return in.read();
-  }
-
-  private void duplicateInputStream(HttpServletRequest request) {
-    try {
-      int len = request.getContentLength();
-      ServletInputStream originalSIS = request.getInputStream();
-      if (len < 0) {
-        in = originalSIS;
-      } else {
-        inputBuffer = new byte[len];
-        int n = originalSIS.read(inputBuffer, 0, len);
-        assert n == len;
-        this.in = new ByteArrayInputStream(inputBuffer);       
-        originalSIS.close();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    TeeServletInputStream(HttpServletRequest request) {
+        duplicateInputStream(request);
     }
-  }
 
-  byte[] getInputBuffer() {
-    return inputBuffer;
-  }
+    @Override
+    public int read() throws IOException {
+        return in.read();
+    }
+
+    private void duplicateInputStream(HttpServletRequest request) {
+        ServletInputStream originalSIS = null;
+        try {
+            originalSIS = request.getInputStream();
+            System.out.println("****************************************************");
+            System.out.println("**************SIS type="+originalSIS.getClass().getName());
+            inputBuffer = consumeBufferAndReturnAsByteArray(originalSIS);
+            this.in = new ByteArrayInputStream(inputBuffer);  
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeStrean(originalSIS);
+        }
+    }
+
+    byte[] consumeBufferAndReturnAsByteArray(InputStream is) throws IOException {
+        int len = 1024;
+        byte[] temp = new byte[len];
+        int c = -1;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        while ((c = is.read(temp, 0, len)) != -1) {
+            baos.write(temp, 0, c);
+        }
+        return baos.toByteArray();
+    }
+
+
+    void closeStrean(ServletInputStream is) {
+        if (is != null) {
+            try {
+                is.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    byte[] getInputBuffer() {
+        return inputBuffer;
+    }
 }
