2e4e829c8e024fa5c51009ba0b507633f5e1adff
Ceki Gulcu <ceki@qos.ch>
2011-02-25 22:53:44 +0100

    added a link to job.html from the header

--- logback-site/src/site/pages/templates/header.js
+++ logback-site/src/site/pages/templates/header.js
@@ -1,7 +1,12 @@
-
-document.write('<p align="left">');
-document.write('<a href="http://logback.qos.ch/">');
+document.write('<table width="100%" border="0"><tr>');
+document.write('<td><a href="http://logback.qos.ch/">');
 document.write('<img src="' + prefix + 'images/logos/lblogo.jpg" alt="" border="0"/>');
-document.write('</a>')
-document.write('</p>');
+document.write('</a></td>')
+
+document.write('<td align="right"><a href="http://logback.qos.ch/job.html">');
+document.write('<img src="' + prefix + 'images/myjob.png" alt="" border="0"/>');
+document.write('</a></td>')
+document.write('</tr></table>')
+
+
 document.write('<div id="headerLine"></div>');
