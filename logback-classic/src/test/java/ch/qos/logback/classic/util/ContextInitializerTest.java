6f1636f14105c3064d41c8f497c15e818f5147c1
Halvard Skogsrud <halvard@skogsrud.com>
2011-12-20 15:34:19 +1100

    Ensure that exception is thrown if configuration file has unexpected extension (not .xml or .groovy)

--- logback-classic/src/test/java/ch/qos/logback/classic/util/ContextInitializerTest.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/util/ContextInitializerTest.java
@@ -14,28 +14,29 @@
 package ch.qos.logback.classic.util;
 
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.slf4j.LoggerFactory;
-
 import ch.qos.logback.classic.ClassicTestConstants;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.ConsoleAppender;
+import ch.qos.logback.core.CoreConstants;
+import ch.qos.logback.core.LogbackException;
 import ch.qos.logback.core.joran.spi.JoranException;
 import ch.qos.logback.core.status.StatusListener;
 import ch.qos.logback.core.status.TrivialStatusListener;
+import ch.qos.logback.core.util.Loader;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class ContextInitializerTest {
 
@@ -119,5 +120,43 @@ public class ContextInitializerTest {
     doAutoConfigFromSystemProperties(ClassicTestConstants.INPUT_PREFIX + "autoConfig.xml");
     sll = lc.getStatusManager().getCopyOfStatusListenerList();
     assertTrue(sll.size() +" should be 1", sll.size() == 1);
+  }
+
+  @Test
+  public void shouldConfigureFromXmlFile() throws MalformedURLException, JoranException {
+    LoggerContext loggerContext = new LoggerContext();
+    ContextInitializer initializer = new ContextInitializer(loggerContext);
+    assertNull(loggerContext.getObject(CoreConstants.SAFE_JORAN_CONFIGURATION));
+
+    URL configurationFileUrl = Loader.getResource("BOO_logback-test.xml", Thread.currentThread().getContextClassLoader());
+    initializer.configureByResource(configurationFileUrl);
+
+    assertNotNull(loggerContext.getObject(CoreConstants.SAFE_JORAN_CONFIGURATION));
+  }
+
+  @Test
+  public void shouldConfigureFromGroovyScript() throws MalformedURLException, JoranException {
+    LoggerContext loggerContext = new LoggerContext();
+    ContextInitializer initializer = new ContextInitializer(loggerContext);
+    assertNull(loggerContext.getObject(CoreConstants.CONFIGURATION_WATCH_LIST));
+
+    URL configurationFileUrl = Loader.getResource("test.groovy", Thread.currentThread().getContextClassLoader());
+    initializer.configureByResource(configurationFileUrl);
+
+    assertNotNull(loggerContext.getObject(CoreConstants.CONFIGURATION_WATCH_LIST));
+  }
+
+  @Test
+  public void shouldThrowExceptionIfUnexpectedConfigurationFileExtension() throws JoranException {
+    LoggerContext loggerContext = new LoggerContext();
+    ContextInitializer initializer = new ContextInitializer(loggerContext);
+
+    URL configurationFileUrl = Loader.getResource("README.txt", Thread.currentThread().getContextClassLoader());
+    try {
+      initializer.configureByResource(configurationFileUrl);
+      fail("Should throw LogbackException");
+    } catch (LogbackException expectedException) {
+      // pass
+    }
   }
 }
