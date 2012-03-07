add5b0681c0b7fc757f5b852da0b89d00e1d2148
Ceki Gulcu <ceki@qos.ch>
2012-03-07 18:19:30 +0100

    ongoing work on LBCORE-234

--- logback-core/src/main/java/ch/qos/logback/core/pattern/PatternLayoutBase.java
+++ logback-core/src/main/java/ch/qos/logback/core/pattern/PatternLayoutBase.java
@@ -32,7 +32,12 @@ abstract public class PatternLayoutBase<E> extends LayoutBase<E> {
   String pattern;
   protected PostCompileProcessor<E> postCompileProcessor;
 
+
   Map<String, String> instanceConverterMap = new HashMap<String, String>();
+
+
+
+  boolean forceEmptyPresentationHeader = false;
 
   /**
    * Concrete implementations of this class are responsible for elaborating the
@@ -142,8 +147,19 @@ abstract public class PatternLayoutBase<E> extends LayoutBase<E> {
     return CoreConstants.EMPTY_STRING;
   }
 
+  public boolean isForceEmptyPresentationHeader() {
+    return forceEmptyPresentationHeader;
+  }
+
+  public void setForceEmptyPresentationHeader(boolean forceEmptyPresentationHeader) {
+    this.forceEmptyPresentationHeader = forceEmptyPresentationHeader;
+  }
+  
   @Override
   public String getPresentationHeader() {
-    return getPresentationHeaderPrefix()+pattern;
+    if(forceEmptyPresentationHeader)
+      return null;
+    else
+      return getPresentationHeaderPrefix()+pattern;
   }
 }
