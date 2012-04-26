7286a1b515a98ff515e663f2d4cec776337bee81
Ceki Gulcu <ceki@qos.ch>
2012-04-26 15:20:26 +0200

    1.0.2 release

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ mvn install
 mvn site:site
 #mvn javadoc:jar
 mvn assembly:assembly
-mvn deploy -P javadocjar
+mvn deploy -P javadocjar,sign-artifacts
 mvn site:deploy -N
 
 #git tag -a v_${VERSION_NUMBER}
