c56ba455ed76a86046844050f59c91b0047d38b6
Ceki Gulcu <ceki@qos.ch>
2012-05-09 10:50:55 +0200

    remove header about metadata on maven central

--- logback-site/src/site/pages/templates/header.js
+++ logback-site/src/site/pages/templates/header.js
@@ -5,8 +5,6 @@ document.write('</a></td>')
 
 document.write('<td>&nbsp;</td>');
 
-document.write('<td width="70%" style="padding-right: 10em;"><span style="font-weight: bold;  font-size: medium;  background-color: #FFF8E8; ">Due to an error, <a href="http://repo1.maven.org/maven2/ch/qos/logback/logback-classic/maven-metadata.xml">metadata</a> for logback on Maven central only mentions versions 1.0.2 and 1.0.3. It follows that "mvn" can no longer fetch earlier versions of logback artifacts from Maven central. Until this issue is resolved, one simple workaround is to upgrade your project&apos;s <em>pom.xml</em> file to use logback version 1.0.3 (or 1.0.2). Please also vote for <a href="https://issues.sonatype.org/browse/OSSRH-3437">OSSRH-3437</a>.</span></td>');
-
 document.write('</tr></table>')
 
 
