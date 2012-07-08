bd6f6c22e1fb2786d0970825e2dffbabda483bf8
Ceki Gulcu <ceki@qos.ch>
2012-07-08 23:35:21 +0200

    automated link anchoring

---
+++ logback-site/src/site/pages/js/decorator.js
@@ -0,0 +1,62 @@
+
+
+//   <h3><a name="LoggerContext" href="#LoggerContext"><span
+//    class="anchor"/></a>Logger context</h3>
+
+function decorate() {
+  decoratePropertiesInTables();
+  decorateDoAnchor();
+}
+
+function decoratePropertiesInTables() {
+ //if(1==1) return;
+ var elems = $('tr td:first-child span.prop');
+
+ for(var i = 0; i < elems.length; i++) {
+   var e = elems[i];
+   var p = e.parentNode;
+   if(p == null) continue;
+
+   var tmpHTML = p.innerHTML;
+   var propName = e.innerHTML;
+   var nameAttr = $(e).attr('name')
+   
+   if(nameAttr == null) {
+     var containerAttr = $(e).attr('container')
+     if(containerAttr != null) 
+       nameAttr = containerAttr+capitaliseFirstLetter(propName);
+     else 
+       nameAttr = propName;
+   }
+  
+   p.innerHTML = "<a name='" + nameAttr + "' href='#" + nameAttr +
+                "'><span class='anchor'/></a><b>" +tmpHTML +"</b>";
+ } // for 
+}
+
+function decorateDoAnchor() {
+   var elems = $('.doAnchor');
+   for(var i = 0; i < elems.length; i++) {
+     var e = elems[i];
+     var tmpHTML = e.innerHTML;
+     var nameAttr = $(e).attr('name')
+     if(nameAttr == null) {
+       nameAttr = camelCase($.trim(tmpHTML))
+     }
+     e.innerHTML = "<a name='" + nameAttr + "' href='#" + nameAttr +
+                "'><span class='anchor'/></a>" +tmpHTML;
+   }
+} 
+
+function capitaliseFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+function camelCase(str) {  
+  var res = str.trim().replace(/\s\w/g, function(match) {
+              return match.trim().toUpperCase();
+            });
+  return res.charAt(0).toLowerCase() + res.slice(1);
+}
+
--- logback-site/src/site/pages/manual/menu.js
+++ logback-site/src/site/pages/manual/menu.js
@@ -1,15 +1,15 @@
 
 document.write('<p class="menu_header">Chapter Index</p>')
 document.write('<p class="menu"><a href="introduction.html"><b>Ch1: Introduction to logback</b></a></p>');
-document.write('<p class="menu"><a href="architecture.html"><b>Ch2: Architecture</b></a>');
-document.write('<p class="menu"><a href="configuration.html"><b>Ch3: Configuration</b></a>');
-document.write('<p class="menu"><a href="appenders.html"><b>Ch4: Appenders</b></a>');
-document.write('<p class="menu"><a href="encoders.html"><b>Ch5: Encoders</b></a>');
-document.write('<p class="menu"><a href="layouts.html"><b>Ch6: Layouts</b></a>');
-document.write('<p class="menu"><a href="filters.html"><b>Ch7: Filters</b></a>');
-document.write('<p class="menu"><a href="mdc.html"><b>Ch8: Mapped Diagnostic Contexts</b></a>');
-document.write('<p class="menu"><a href="loggingSeparation.html"><b>Ch9: Logging Separation</b></a>');
-document.write('<p class="menu"><a href="jmxConfig.html"><b>Ch10: JMX Configurator</b></a>');
-document.write('<p class="menu"><a href="onJoran.html"><b>Ch11: Joran</b></a>');
-document.write('<p class="menu"><a href="groovy.html"><b>Ch12: Groovy Configuration</b></a>');
-document.write('<p class="menu"><a href="migrationFromLog4j.html"><b>Ch13: Migration from log4j</b></a>');
+document.write('<p class="menu"><a href="architecture.html"><b>Ch2: Architecture</b></a></p>');
+document.write('<p class="menu"><a href="configuration.html"><b>Ch3: Configuration</b></a></p>');
+document.write('<p class="menu"><a href="appenders.html"><b>Ch4: Appenders</b></a></p>');
+document.write('<p class="menu"><a href="encoders.html"><b>Ch5: Encoders</b></a></p>');
+document.write('<p class="menu"><a href="layouts.html"><b>Ch6: Layouts</b></a></p>');
+document.write('<p class="menu"><a href="filters.html"><b>Ch7: Filters</b></a></p>');
+document.write('<p class="menu"><a href="mdc.html"><b>Ch8: Mapped Diagnostic Contexts</b></a></p>');
+document.write('<p class="menu"><a href="loggingSeparation.html"><b>Ch9: Logging Separation</b></a></p>');
+document.write('<p class="menu"><a href="jmxConfig.html"><b>Ch10: JMX Configurator</b></a></p>');
+document.write('<p class="menu"><a href="onJoran.html"><b>Ch11: Joran</b></a></p>');
+document.write('<p class="menu"><a href="groovy.html"><b>Ch12: Groovy Configuration</b></a></p>');
+document.write('<p class="menu"><a href="migrationFromLog4j.html"><b>Ch13: Migration from log4j</b></a></p>');
