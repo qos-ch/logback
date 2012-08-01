bc12a3f21ad2c0d2c8f77a89f21e8313c5fb64e2
Brian Campbell <brian.campbell@mckesson.com>
2012-08-01 04:10:45 -0400

    LOGBACK-729: Currently there is no support for nested variables/options

--- logback-core/src/test/java/ch/qos/logback/core/util/OptionHelperTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/util/OptionHelperTest.java
@@ -109,6 +109,106 @@ public class OptionHelperTest  {
     assertEquals(expected, result); 
   }
 
+  String nest1 = "${X1}ABC"; //Y1 => Y1ABC
+  String X1 = "Y1";
+  String pass1 = "Y1ABC";
+  @Test
+  public void testNestedVars1() {
+    context.putProperty("X1", X1);
+
+    String result = OptionHelper.substVars(nest1, context);
+    assertEquals(pass1, result);
+  }
+
+  String nest2 = "${${X1}2}ABC"; //Y1 => Y12 => Z34 => Z34ABC
+  String Y12 = "Z34";
+  String pass2 = "Z34ABC";
+  @Test
+  public void testNestedVars2() {
+    context.putProperty("X1", X1);
+    context.putProperty("Y12", Y12);
+
+    String result = OptionHelper.substVars(nest2, context);
+    assertEquals(pass2, result);
+  }
+
+  String nest3 = "${${${X1}}}ABC"; //Y1 => Z1 => Q1 => Q1ABC
+  String Y1 = "Z1";
+  String Z1 = "Q1";
+  String pass3 = "Q1ABC";
+  @Test
+  public void testNestedVars3() {
+    context.putProperty("X1", X1);
+    context.putProperty("Y1", Y1);
+    context.putProperty("Z1", Z1);
+
+    String result = OptionHelper.substVars(nest3, context);
+    assertEquals(pass3, result);
+  }
+
+  String nest4 = "${${X1}${X2}}ABC"; //Y1Y2 => Z1Z2 => Z1Z2ABC
+  String X2 = "Y2";
+  String Y1Y2 = "Z1Z2";
+  String pass4 = "Z1Z2ABC";
+  @Test
+  public void testNestedVars4() {
+    context.putProperty("X1", X1);
+    context.putProperty("X2", X2);
+    context.putProperty("Y1Y2", Y1Y2);
+
+    String result = OptionHelper.substVars(nest4, context);
+    assertEquals(pass4, result);
+  }
+
+  String nest5 = "${A${X1}}ABC"; //Y1 => AY1 => BZ2 => BZ2ABC
+  String AY1 = "BZ2";
+  String pass5 = "BZ2ABC";
+  @Test
+  public void testNestedVars5() {
+    context.putProperty("X1", X1);
+    context.putProperty("AY1", AY1);
+
+    String result = OptionHelper.substVars(nest5, context);
+    assertEquals(pass5, result);
+  }
+
+  String nest6 = "${A${X1}B}CDE"; //Y1 => AY1B => BZ2C => BZ2CCDE
+  String AY1B = "BZ2C";
+  String pass6 = "BZ2CCDE";
+  @Test
+  public void testNestedVars6() {
+    context.putProperty("X1", X1);
+    context.putProperty("AY1B", AY1B);
+
+    String result = OptionHelper.substVars(nest6, context);
+    assertEquals(pass6, result);
+  }
+
+  String nest7 = "${A${X1}B${X2}}CDE"; //Y1,Y2 => AY1BY2 => BZ2CZ3 => BZ2CZ3CDE
+  String AY1BY2 = "BZ2CZ3";
+  String pass7 = "BZ2CZ3CDE";
+  @Test
+  public void testNestedVars7() {
+    context.putProperty("X1", X1);
+    context.putProperty("X2", X2);
+    context.putProperty("AY1BY2", AY1BY2);
+
+    String result = OptionHelper.substVars(nest7, context);
+    assertEquals(pass7, result);
+  }
+
+  String nest8 = "${A${X1}B${X2}C}DEF"; //Y1,Y2 => AY1BY2C => BZ2CZ3D => BZ2CZ3DDEF
+  String AY1BY2C = "BZ2CZ3D";
+  String pass8 = "BZ2CZ3DDEF";
+  @Test
+  public void testNestedVars8() {
+    context.putProperty("X1", X1);
+    context.putProperty("X2", X2);
+    context.putProperty("AY1BY2C", AY1BY2C);
+
+    String result = OptionHelper.substVars(nest8, context);
+    assertEquals(pass8, result);
+  }
 
   @Ignore
   @Test
