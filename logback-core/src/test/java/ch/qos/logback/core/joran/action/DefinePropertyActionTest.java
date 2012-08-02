6e652c781abd2c4e0b11150d89dfc7354165c6b7
Ceki Gulcu <ceki@qos.ch>
2012-08-02 22:58:31 +0200

    docs on scopes

--- logback-core/src/test/java/ch/qos/logback/core/joran/action/DefinePropertyActionTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/joran/action/DefinePropertyActionTest.java
@@ -71,7 +71,6 @@ public class DefinePropertyActionTest {
   public void good() throws JoranException {
     simpleConfigurator.doConfigure(DEFINE_INPUT_DIR + GOOD_XML);
     InterpretationContext ic = simpleConfigurator.getInterpreter().getInterpretationContext();
-    // get from the InterpretationContext
     String inContextFoo = ic.getProperty("foo");
     assertEquals("monster", inContextFoo);
   }
