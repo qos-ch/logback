74d21905195f73230accc060d5aab5678e47f4c0
Ceki Gulcu <ceki@qos.ch>
2012-08-09 17:26:19 +0200

    minor doc edits

--- logback-site/src/site/pages/templates/footer.js
+++ logback-site/src/site/pages/templates/footer.js
@@ -5,9 +5,9 @@ document.write('<tr>')
 
 document.write('<td valign="top">Copyright &copy; 2012  <a href="http://www.qos.ch/">QOS.ch</a></td>')
 
-document.write('  <td rowspan="2">')
-document.write('    <script type="text/javascript" src="http://www.ohloh.net/p/13616/widgets/project_users.js"></script>')
-document.write('  </td>')
+//document.write('  <td rowspan="2">')
+//document.write('    <script type="text/javascript" src="http://www.ohloh.net/p/13616/widgets/project_users.js"></script>')
+//document.write('  </td>')
 
 
 document.write('</tr>')
