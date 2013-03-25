9ccc2c3f933430375b1552f2da2e471be17e646d
Carl Harris <ceharris@vt.edu>
2013-03-25 19:12:04 -0400

    Revert "added SSLConfiguration.createParameters"
    This reverts commit d1dca0eb1cb3209848e15cfeb3deab7dbf7d90e4.

--- logback-classic/src/main/java/ch/qos/logback/classic/net/ssl/SSLConfiguration.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/net/ssl/SSLConfiguration.java
@@ -14,9 +14,6 @@
 package ch.qos.logback.classic.net.ssl;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLParameters;
-
-import ch.qos.logback.core.spi.ContextAware;
 
 /**
  * A configuration for an {@link SSLContext}.
@@ -42,22 +39,6 @@ public class SSLConfiguration extends SSLContextFactoryBean {
    */
   public void setParameters(SSLParametersFactoryBean parameters) {
     this.parameters = parameters;
-  }
-
-  /**
-   * Creates an {@link SSLParameters} according to the receiver's 
-   * SSL parameters configuration or the context's default parameters if
-   * no configuration has been specified.
-   * @param sslContext SSL context
-   * @return parameters object
-   */
-  public SSLParameters createParameters(SSLContext sslContext,
-      ContextAware context) {
-    if (getParameters() != null) {
-      return getParameters().createSSLParameters(
-        sslContext.getDefaultSSLParameters(), context);
-    }
-    return sslContext.getDefaultSSLParameters();
   }
 
 }
