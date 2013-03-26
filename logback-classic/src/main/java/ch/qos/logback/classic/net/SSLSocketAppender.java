954a7608a72ba325165411ab81d7361ae122e885
Carl Harris <ceharris@vt.edu>
2013-03-26 07:08:53 -0400

    added SSLSocketAppender and corresponding SimpleSSLSocketServer

---
+++ logback-classic/src/main/java/ch/qos/logback/classic/net/SSLSocketAppender.java
@@ -0,0 +1,70 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2011, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+package ch.qos.logback.classic.net;
+
+import java.net.InetAddress;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.net.SSLSocketAppenderBase;
+import ch.qos.logback.core.spi.PreSerializationTransformer;
+
+/**
+ * A {@link SocketAppender} that supports SSL.
+ * <p>
+ * For more information on this appender, please refer to the online manual
+ * at http://logback.qos.ch/manual/appenders.html#SSLSocketAppender
+ * 
+ * @author Carl Harris
+ */
+public class SSLSocketAppender extends SSLSocketAppenderBase<ILoggingEvent> {
+
+  private final PreSerializationTransformer<ILoggingEvent> pst = 
+      new LoggingEventPreSerializationTransformer();
+
+  private boolean includeCallerData;
+  
+  public SSLSocketAppender() {
+  }
+
+  /**
+   * Connects to remote server at <code>address</code> and <code>port</code>.
+   */
+  public SSLSocketAppender(String host, int port) {
+    this(getAddressByName(host), port);
+  }
+
+  /**
+   * Connects to remote server at <code>address</code> and <code>port</code>.
+   */
+  public SSLSocketAppender(InetAddress address, int port) {
+    this.address = address;
+    this.port = port;
+  }
+
+  @Override
+  protected void postProcessEvent(ILoggingEvent event) {
+    if (includeCallerData) {
+      event.getCallerData();
+    }
+  }
+
+  public void setIncludeCallerData(boolean includeCallerData) {
+    this.includeCallerData = includeCallerData;
+  }
+  
+  public PreSerializationTransformer<ILoggingEvent> getPST() {
+    return pst;
+  }
+   
+}
---
+++ logback-classic/src/main/java/ch/qos/logback/classic/net/SimpleSSLSocketServer.java
@@ -0,0 +1,122 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2011, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+package ch.qos.logback.classic.net;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.net.ssl.ConfigurableSSLServerSocketFactory;
+
+/**
+ * A {@link SimpleSocketServer} that supports SSL.
+ * 
+ * <pre>
+ *      &lt;b&gt;Usage:&lt;/b&gt; java ch.qos.logback.classic.net.ssl.SimpleSSLSocketServer port configFile
+ * </pre>
+ * 
+ * where <em>port</em> is a port number where the server listens and
+ * <em>configFile</em> is an xml configuration file fed to
+ * {@link JoranConfigurator}.
+ * 
+ * </pre>
+ * 
+ * @author Carl Harris
+ */
+public class SimpleSSLSocketServer extends SimpleSocketServer {
+
+  private final ServerSocketFactory socketFactory;
+
+  /**
+   * Runs a SimpleSSLSocketServer using the default SSL context.
+   * <p>
+   * When running the SimpleSSLServerFactory, it is necessary to configure 
+   * JSSE system properties using {@code -Dname=value} on the command-line 
+   * when starting the server.  In particular, you will probably want/need
+   * to configure the following system properties:
+   * <ul>
+   *   <li>javax.net.ssl.keyStore</li>
+   *   <li>javax.net.ssl.keyStorePassword</li>
+   *   <li>javax.net.ssl.keyStoreType</li>
+   *   <li>javax.net.ssl.trustStore</li>
+   *   <li>javax.net.ssl.trustStorePassword</li>
+   *   <li>javax.net.ssl.trustStoreType</li>
+   * </ul>
+   * <p>
+   * See the <a href="http://docs.oracle.com/javase/1.5.0/docs/guide/security/jsse/JSSERefGuide.html#InstallationAndCustomization">
+   * Customizing the JSSE</a> in the JSSE Reference Guide for details on
+   * how to set these system properties.
+   * 
+   * @param argv command line arguments;
+   *    {@code argv[0]} is the port on which the server should listen
+   *    {@code argv[1]} is a path to the logger configuration file
+   * @throws Exception
+   */
+  public static void main(String argv[]) throws Exception {
+    doMain(SimpleSSLSocketServer.class, argv);
+  }
+
+  /**
+   * Creates a new server using the default SSL context.
+   * @param lc logger context for received events
+   * @param port port on which the server is to listen
+   * @throws NoSuchAlgorithmException if the default SSL context cannot
+   *    be created
+   */
+  public SimpleSSLSocketServer(LoggerContext lc, int port) 
+      throws NoSuchAlgorithmException {
+    this(lc, port, SSLContext.getDefault());
+  }
+
+  /**
+   * Creates a new server using a custom SSL context.
+   * @param lc logger context for received events
+   * @param port port on which the server is to listen
+   * @param context custom SSL context
+   */
+  public SimpleSSLSocketServer(LoggerContext lc, int port,
+      SSLContext context) {
+    this(lc, port, context, context.getDefaultSSLParameters());
+  }
+  
+  /**
+   * Creates a new server using custom SSL context and parameters.
+   * @param lc logger context for received events
+   * @param port port on which the server is to listen
+   * @param context custom SSL context
+   * @param parameters custom SSL parameters
+   */
+  public SimpleSSLSocketServer(LoggerContext lc, int port,
+      SSLContext context, SSLParameters parameters) {
+    super(lc, port);
+    if (context == null) {
+      throw new NullPointerException("SSL context required");
+    }
+    if (parameters == null) {
+      throw new NullPointerException("SSL parameters required");
+    }
+    this.socketFactory = new ConfigurableSSLServerSocketFactory(
+        parameters, context.getServerSocketFactory());
+  }
+  
+  @Override
+  protected ServerSocketFactory getServerSocketFactory() {
+    return socketFactory;
+  }
+
+}
