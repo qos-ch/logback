39481b77adf5f371da95acb294cc86ce0e89a7cd
Carl Harris <ceharris@vt.edu>
2013-04-02 04:22:49 -0400

    added SSLServerSocketAppenderBase and associated test class

---
+++ logback-core/src/main/java/ch/qos/logback/core/net/server/SSLServerSocketAppenderBase.java
@@ -0,0 +1,79 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2011, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+package ch.qos.logback.core.net.server;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLContext;
+
+import ch.qos.logback.core.net.ssl.ConfigurableSSLServerSocketFactory;
+import ch.qos.logback.core.net.ssl.SSLConfiguration;
+import ch.qos.logback.core.net.ssl.SSLParametersConfiguration;
+
+/**
+ * 
+ * This is the base class for module specific ServerSocketAppender 
+ * implementations.
+ * 
+ * @author Carl Harris
+ */
+public abstract class SSLServerSocketAppenderBase<E> 
+    extends ServerSocketAppenderBase<E> {
+
+  private SSLConfiguration ssl;
+  private ServerSocketFactory socketFactory;
+  
+  @Override
+  protected ServerSocketFactory getServerSocketFactory() {
+    return socketFactory;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void start() {
+    try {
+      SSLContext sslContext = getSsl().createContext(this);
+      SSLParametersConfiguration parameters = getSsl().getParameters();
+      parameters.setContext(getContext());
+      socketFactory = new ConfigurableSSLServerSocketFactory(parameters, 
+          sslContext.getServerSocketFactory());
+      super.start();
+    }
+    catch (Exception ex) {
+      addError(ex.getMessage(), ex);
+    }
+  }
+
+  /**
+   * Gets the SSL configuration.
+   * @return SSL configuration; if no configuration has been set, a
+   *    default configuration is returned
+   */
+  public SSLConfiguration getSsl() {
+    if (ssl == null) {
+      ssl = new SSLConfiguration();
+    }
+    return ssl;
+  }
+
+  /**
+   * Sets the SSL configuration.
+   * @param ssl the SSL configuration to set
+   */
+  public void setSsl(SSLConfiguration ssl) {
+    this.ssl = ssl;
+  }
+  
+}
---
+++ logback-core/src/test/java/ch/qos/logback/core/net/server/SSLServerSocketAppenderBaseTest.java
@@ -0,0 +1,62 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2011, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+package ch.qos.logback.core.net.server;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.qos.logback.core.net.mock.MockContext;
+import ch.qos.logback.core.spi.PreSerializationTransformer;
+
+/**
+ * Unit tests for {@link SSLServerSocketAppenderBase}.
+ *
+ * @author Carl Harris
+ */
+public class SSLServerSocketAppenderBaseTest {
+
+  private MockContext context = new MockContext();
+  private SSLServerSocketAppenderBase appender =
+      new InstrumentedSSLServerSocketAppenderBase();
+  
+  @Before
+  public void setUp() throws Exception {
+    appender.setContext(context);
+  }
+  
+  @Test
+  public void testUsingDefaultConfig() throws Exception {
+    // should be able to start successfully with no SSL configuration at all
+    appender.start();
+    assertNotNull(appender.getServerSocketFactory());
+  }
+
+  private static class InstrumentedSSLServerSocketAppenderBase 
+      extends SSLServerSocketAppenderBase<Object> {
+
+    @Override
+    protected void postProcessEvent(Object event) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected PreSerializationTransformer<Object> getPST() {
+      throw new UnsupportedOperationException();
+    }
+    
+  }
+  
+}
