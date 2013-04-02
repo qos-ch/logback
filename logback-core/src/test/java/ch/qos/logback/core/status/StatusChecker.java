e15a680ac203006a0102e7827c638828ac01cec5
Ceki Gulcu <ceki@qos.ch>
2013-04-02 21:27:44 +0200

    added missing StatusChecker class

---
+++ logback-core/src/test/java/ch/qos/logback/core/status/StatusChecker.java
@@ -0,0 +1,51 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2013, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+
+package ch.qos.logback.core.status;
+
+import ch.qos.logback.core.Context;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Extend  StatusUtil with assertions.
+ */
+public class StatusChecker extends StatusUtil {
+
+  public StatusChecker(StatusManager sm) {
+    super(sm);
+  }
+
+  public StatusChecker(Context context) {
+    super(context);
+  }
+
+  public void assertContainsMatch(int level, String regex) {
+    assertTrue(containsMatch(level, regex));
+  }
+
+  public void assertContainsMatch(String regex) {
+    assertTrue(containsMatch(regex));
+  }
+
+  public void asssertContainsException(Class scanExceptionClass) {
+    assertTrue(containsException(scanExceptionClass));
+  }
+
+  public void assertIsErrorFree() {
+    assertTrue(isErrorFree(0));
+  }
+
+
+}
