38a5b7a43ea58a5a1da3e1f9fcbbe37213dbd769
Carl Harris <ceharris@vt.edu>
2013-04-02 11:44:19 -0400

    Completed implementation of ServerSocketAppenderBase
    Implemented lifecyle and appender methods, and added unit tests and
    supporting classes.

--- logback-core/src/main/java/ch/qos/logback/core/net/server/ServerSocketAppenderBase.java
+++ logback-core/src/main/java/ch/qos/logback/core/net/server/ServerSocketAppenderBase.java
@@ -13,9 +13,17 @@
  */
 package ch.qos.logback.core.net.server;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.util.concurrent.Executor;
+
 import javax.net.ServerSocketFactory;
 
 import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.net.SocketAppenderBase;
 import ch.qos.logback.core.spi.PreSerializationTransformer;
 
 /**
@@ -28,18 +36,84 @@ import ch.qos.logback.core.spi.PreSerializationTransformer;
 public abstract class ServerSocketAppenderBase<E> extends AppenderBase<E> {
 
   /**
-   * Gets the factory used to create {@link ServerSocket} objects.
-   * <p>
-   * The default implementation delegates to 
-   * {@link ServerSocketFactory#getDefault()}.  Subclasses may override to
-   * private a different socket factory implementation.
-   * 
-   * @return socket factory.
+   * Default {@link ServerSocket} backlog
    */
-  protected ServerSocketFactory getServerSocketFactory() {
-    return ServerSocketFactory.getDefault();
+  public static final int DEFAULT_BACKLOG = 50;
+
+  /** 
+   * Default queue size used for each client
+   */
+  public static final int DEFAULT_CLIENT_QUEUE_SIZE = 100;
+  
+  private int port = SocketAppenderBase.DEFAULT_PORT;
+  private int backlog = DEFAULT_BACKLOG;
+  private int clientQueueSize = DEFAULT_CLIENT_QUEUE_SIZE;
+  
+  private String address;
+  private ThreadPoolFactoryBean threadPool;
+
+  private ServerRunner<RemoteLoggerClient> runner;
+
+  @Override
+  public void start() {
+    if (isStarted()) return;
+    try {
+      ServerSocket socket = getServerSocketFactory().createServerSocket(
+          getPort(), getBacklog(), getInetAddress());    
+      ServerListener<RemoteLoggerClient> listener = createServerListener(socket);
+      runner = createServerRunner(listener, getThreadPool().createExecutor());
+      runner.setContext(getContext());
+      runner.start();
+    }
+    catch (Exception ex) {
+      addError("server startup error: " + ex, ex);
+    }
   }
   
+  protected ServerListener<RemoteLoggerClient> createServerListener(
+      ServerSocket socket) {
+    return new RemoteLoggerServerListener(socket);
+  }
+  
+  protected ServerRunner<RemoteLoggerClient> createServerRunner(
+      ServerListener<RemoteLoggerClient> listener,
+      Executor executor) {
+    return new RemoteLoggerServerRunner(listener, executor, 
+        getClientQueueSize());
+  }
+  
+  @Override
+  public void stop() {
+    if (!isStarted()) return;
+    try {
+      runner.stop();
+    }
+    catch (IOException ex) {
+      addError("server shutdown error: " + ex, ex);
+    }
+  }
+
+  /**
+   * Gets a flag indicating whether the server is running.
+   * @return flag state
+   */
+  @Override
+  public final boolean isStarted() {
+    return runner != null && runner.isStarted();
+  }
+
+  @Override
+  protected void append(E event) {
+    if (event == null) return;
+    postProcessEvent(event);
+    final Serializable serEvent = getPST().transform(event);
+    runner.accept(new ClientVisitor<RemoteLoggerClient>() {
+      public void visit(RemoteLoggerClient client) {
+        client.offer(serEvent);
+      }      
+    });
+  }
+
   /**
    * Post process an event received via {@link #append(E)}.
    * @param event
@@ -53,20 +127,119 @@ public abstract class ServerSocketAppenderBase<E> extends AppenderBase<E> {
    */
   protected abstract PreSerializationTransformer<E> getPST();
 
-
-  @Override
-  public void start() {
-
+  /**
+   * Gets the factory used to create {@link ServerSocket} objects.
+   * <p>
+   * The default implementation delegates to 
+   * {@link ServerSocketFactory#getDefault()}.  Subclasses may override to
+   * private a different socket factory implementation.
+   * 
+   * @return socket factory.
+   */
+  protected ServerSocketFactory getServerSocketFactory() throws Exception {
+    return ServerSocketFactory.getDefault();
   }
 
-  @Override
-  public void stop() {
+  /**
+   * Gets the local address for the listener.
+   * @return an {@link InetAddress} representation of the local address.
+   * @throws UnknownHostException
+   */
+  protected InetAddress getInetAddress() throws UnknownHostException {
+    if (getAddress() == null) return null;
+    return InetAddress.getByName(getAddress());
+  }
   
+  /**
+   * Gets the local port for the listener.
+   * @return local port
+   */
+  public int getPort() {
+    return port;
   }
 
-  @Override
-  protected void append(E eventObject) {
+  /**
+   * Sets the local port for the listener.
+   * @param port the local port to set
+   */
+  public void setPort(int port) {
+    this.port = port;
+  }
 
+  /**
+   * Gets the listener queue depth.
+   * <p>
+   * This represents the number of connected clients whose connections 
+   * have not yet been accepted.
+   * @return queue depth
+   * @see java.net.ServerSocket
+   */
+  public Integer getBacklog() {
+    return backlog;
+  }
+
+  /**
+   * Sets the listener queue depth.
+   * <p>
+   * This represents the number of connected clients whose connections 
+   * have not yet been accepted.
+   * @param backlog the queue depth to set
+   * @see java.net.ServerSocket
+   */
+  public void setBacklog(Integer backlog) {
+    this.backlog = backlog;
+  }
+
+  /**
+   * Gets the local address for the listener.
+   * @return a string representation of the local address
+   */
+  public String getAddress() {
+    return address;
+  }
+
+  /**
+   * Sets the local address for the listener.
+   * @param address a host name or a string representation of an IP address
+   */
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  
+  /**
+   * Gets the event queue size used for each client connection. 
+   * @return queue size
+   */
+  public int getClientQueueSize() {
+    return clientQueueSize;
+  }
+
+  /**
+   * Sets the event queue size used for each client connection.
+   * @param clientQueueSize the queue size to set
+   */
+  public void setClientQueueSize(int clientQueueSize) {
+    this.clientQueueSize = clientQueueSize;
+  }
+
+  /**
+   * Gets the server's thread pool configuration.
+   * @return thread pool configuration; if no thread pool configuration was
+   *    provided, a default configuration is returned
+   */
+  public ThreadPoolFactoryBean getThreadPool() {
+    if (threadPool == null) {
+      return new ThreadPoolFactoryBean();
+    }
+    return threadPool;
+  }
+
+  /**
+   * Sets the server's thread pool configuration.
+   * @param threadPool the configuration to set
+   */
+  public void setThreadPool(ThreadPoolFactoryBean threadPool) {
+    this.threadPool = threadPool;
   }
 
 }
---
+++ logback-core/src/test/java/ch/qos/logback/core/net/server/InstrumentedServerSocketAppenderBase.java
@@ -0,0 +1,114 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2011, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+
+package ch.qos.logback.core.net.server;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.util.concurrent.Executor;
+
+import javax.net.ServerSocketFactory;
+
+import ch.qos.logback.core.spi.PreSerializationTransformer;
+
+/**
+ * A {@link ServerSocketAppenderBase} with instrumentation for unit testing.
+ *
+ * @author Carl Harris
+ */
+public class InstrumentedServerSocketAppenderBase
+    extends ServerSocketAppenderBase<Serializable> {
+
+  private final ServerSocket serverSocket;
+  private final ServerListener<RemoteLoggerClient> listener;
+  private final ServerRunner<RemoteLoggerClient> runner;
+  
+  private ServerListener lastListener;
+  private Executor lastExecutor;
+  
+  public InstrumentedServerSocketAppenderBase(ServerSocket serverSocket) {
+    this(serverSocket, new RemoteLoggerServerListener(serverSocket), null);
+  }
+  
+  public InstrumentedServerSocketAppenderBase(ServerSocket serverSocket,
+      ServerListener<RemoteLoggerClient> listener, 
+      ServerRunner<RemoteLoggerClient> runner) {
+    this.serverSocket = serverSocket;
+    this.listener = listener;
+    this.runner = runner;
+  }
+
+  @Override
+  protected void postProcessEvent(Serializable event) {
+  }
+
+  @Override
+  protected PreSerializationTransformer<Serializable> getPST() {
+    return new PreSerializationTransformer<Serializable>() {
+      public Serializable transform(Serializable event) {
+        return event;
+      }
+    };
+  }
+  
+  @Override
+  protected ServerSocketFactory getServerSocketFactory() throws Exception {
+    return new ServerSocketFactory() {
+
+      @Override
+      public ServerSocket createServerSocket(int port) throws IOException {
+        return serverSocket;
+      }
+
+      @Override
+      public ServerSocket createServerSocket(int port, int backlog)
+          throws IOException {
+        return serverSocket;
+      }
+
+      @Override
+      public ServerSocket createServerSocket(int port, int backlog,
+          InetAddress ifAddress) throws IOException {
+        return serverSocket;
+      }        
+    };
+  }
+
+  
+
+  @Override
+  protected ServerRunner<RemoteLoggerClient> createServerRunner(
+      ServerListener<RemoteLoggerClient> listener, Executor executor) {
+    lastListener = listener;
+    lastExecutor = executor;
+    return runner != null ? runner : super.createServerRunner(listener, executor);
+  }
+
+  @Override
+  protected ServerListener<RemoteLoggerClient> createServerListener(
+      ServerSocket socket) {
+    return listener;
+  }
+
+  public ServerListener getLastListener() {
+    return lastListener;
+  }
+
+  public Executor getLastExecutor() {
+    return lastExecutor;
+  }
+
+}
