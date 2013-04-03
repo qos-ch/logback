550675c9dd697ece8aa5e795d7d3f843de8151c9
Carl Harris <ceharris@vt.edu>
2013-04-03 18:47:37 -0400

    added socket (re)connector
    This class reimplements the much of the reconnect logic of the existing
    SocketAppenderBase but does so by exposing an API that is easy to
    integrate in a class that needs to handle automatic outbound connection
    re-establishment, and in a way that is more easily tested.  This should
    be adopted into SocketAppenderBase after some operational experience is
    gained with it.

---
+++ logback-core/src/test/java/ch/qos/logback/core/net/SocketConnectorBaseTest.java
@@ -0,0 +1,125 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2011, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+package ch.qos.logback.core.net;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.net.ConnectException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.qos.logback.core.net.SocketConnector.ExceptionHandler;
+import ch.qos.logback.core.net.server.ServerSocketUtil;
+
+/**
+ * Unit tests for {@link SocketConnectorBase}.
+ *
+ * @author Carl Harris
+ */
+public class SocketConnectorBaseTest {
+
+  private static final int DELAY = 250;
+  
+  private MockExceptionHandler exceptionHandler = new MockExceptionHandler();
+  
+  private ServerSocket serverSocket;
+  private SocketConnectorBase connector;
+  
+  @Before
+  public void setUp() throws Exception {
+    serverSocket = ServerSocketUtil.createServerSocket();
+    connector = new SocketConnectorBase(serverSocket.getInetAddress(), 
+        serverSocket.getLocalPort(), 0, DELAY);
+    connector.setExceptionHandler(exceptionHandler);
+  }
+  
+  @After
+  public void tearDown() throws Exception {
+    if (serverSocket != null) {
+      serverSocket.close();
+    }
+  }
+  
+  @Test
+  public void testConnect() throws Exception {
+    Thread thread = new Thread(connector);
+    thread.start();
+    Socket socket = connector.awaitConnection(2 * DELAY);
+    assertNotNull(socket);
+    assertFalse(thread.isAlive());
+    socket.close();
+  }
+
+  @Test
+  public void testConnectionFails() throws Exception {
+    serverSocket.close();
+    Thread thread = new Thread(connector);
+    thread.start();
+    Socket socket = connector.awaitConnection(2 * DELAY);
+    assertNull(socket);
+    assertTrue(exceptionHandler.getLastException() instanceof ConnectException);
+    assertTrue(thread.isAlive());
+    thread.interrupt();
+    thread.join(1000);
+    assertFalse(thread.isAlive());
+    assertTrue(exceptionHandler.getLastException() instanceof InterruptedException);
+  }
+
+  @Test
+  public void testConnectEventually() throws Exception {
+    serverSocket.close();
+    
+    Thread thread = new Thread(connector);
+    thread.start();
+    Socket socket = connector.awaitConnection(2 * DELAY);
+    assertNull(socket);
+    assertTrue(exceptionHandler.getLastException() instanceof ConnectException);
+    assertTrue(thread.isAlive());
+    
+    // now rebind to the same local address
+    SocketAddress address = serverSocket.getLocalSocketAddress();
+    serverSocket = new ServerSocket();
+    serverSocket.setReuseAddress(true);
+    serverSocket.bind(address);
+    
+    // now we should be able to connect
+    socket = connector.awaitConnection(2 * DELAY);
+    assertNotNull(socket);
+    assertFalse(thread.isAlive());
+    socket.close();
+  }
+  
+  private static class MockExceptionHandler implements ExceptionHandler {
+
+    private Exception lastException;
+    
+    public void connectionFailed(SocketConnector connector, Exception ex) {
+      lastException = ex;
+    }
+    
+    public Exception getLastException() {
+      return lastException;
+    }
+    
+  }
+  
+}
