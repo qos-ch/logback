07fb99b13f9db283f138dfb976846eeb5bac4da7
Ceki Gulcu <ceki@qos.ch>
2013-04-18 13:50:44 +0200

    update license headers

--- logback-core/src/test/java/ch/qos/logback/core/net/server/ThreadPoolFactoryBeanTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/net/server/ThreadPoolFactoryBeanTest.java
@@ -1,20 +1,15 @@
-/*
- * File created on Apr 8, 2013 
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2013, QOS.ch. All rights reserved.
  *
- * Copyright 2008-2011 Virginia Polytechnic Institute and State University
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   or (per the licensee's choosing)
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
  */
 package ch.qos.logback.core.net.server;
 
--- logback-core/src/test/java/ch/qos/logback/core/read/CyclicBufferAppenderTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/read/CyclicBufferAppenderTest.java
@@ -1,7 +1,6 @@
 /**
  * Logback: the reliable, generic, fast and flexible logging framework.
- *
- * Copyright 2013 SURFnet bv, The Netherlands
+ * Copyright (C) 1999-2013, QOS.ch. All rights reserved.
  *
  * This program and the accompanying materials are dual-licensed under
  * either the terms of the Eclipse Public License v1.0 as published by
