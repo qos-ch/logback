879081248675a22013663a51a629ba2bcedfd094
Ceki Gulcu <ceki@qos.ch>
2013-04-25 22:38:26 +0200

    more unit tests

--- logback-core/src/test/java/ch/qos/logback/core/sift/AppenderTrackerTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/sift/AppenderTrackerTest.java
@@ -13,11 +13,9 @@
  */
 package ch.qos.logback.core.sift;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
+import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.testUtil.RandomUtil;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,37 +23,112 @@ import ch.qos.logback.core.Context;
 import ch.qos.logback.core.ContextBase;
 import ch.qos.logback.core.read.ListAppender;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class AppenderTrackerTest {
 
-
-  AppenderFactory a;
-
   Context context = new ContextBase();
-  AppenderTracker<Object> appenderTracker = new AppenderTracker<Object>(context, null);
-  ListAppender<Object> la = new ListAppender<Object>();
-  String key = "a";
-  
+  ListAppenderFactory listAppenderFactory = new ListAppenderFactory();
+  int diff = RandomUtil.getPositiveInt();
+  AppenderTracker<Object> appenderTracker = new AppenderTracker<Object>(context, listAppenderFactory);
+  String key = "k-" + diff;
+  long now = 3000;
+
   @Before
   public void setUp() {
-    la.setContext(context);
-    la.start();
   }
 
   @Test
-  public void empty0() {
-    long now = 3000;
+  public void removeStaleComponentsShouldNotBomb() {
     appenderTracker.removeStaleComponents(now);
     assertEquals(0, appenderTracker.getComponentCount());
   }
 
   @Test
-  public void empty1() {
-    long now = 3000;
+  public void findingTheInexistentShouldNotBomb() {
     assertNull(appenderTracker.find(key));
-    now += AppenderTracker.DEFAULT_TIMEOUT+1000;
+    now += AppenderTracker.DEFAULT_TIMEOUT + 1000;
     appenderTracker.removeStaleComponents(now);
     assertNull(appenderTracker.find(key));
   }
 
+  @Test
+  public void smoke() {
+    appenderTracker.getOrCreate(key, now);
+    Appender<Object> a = appenderTracker.find(key);
+    assertTrue(a.isStarted());
+    now += AppenderTracker.DEFAULT_TIMEOUT + 1000;
+    appenderTracker.removeStaleComponents(now);
+    assertFalse(a.isStarted());
+    assertNull(appenderTracker.find(key));
+  }
 
+  @Test
+  public void endOfLivedItemsShouldBeRemovedAfterLingeringTimeout() {
+    Appender a = appenderTracker.getOrCreate(key, now);
+    appenderTracker.endOfLife(key);
+    now += AppenderTracker.LINGERING_TIMEOUT + 1;
+    appenderTracker.removeStaleComponents(now);
+    assertFalse(a.isStarted());
+    a = appenderTracker.find(key);
+    assertNull(a);
+  }
+
+  @Test
+  public void trackerShouldHonorMaxComponentsParameter() {
+    List<Appender<Object>> appenderList = new ArrayList<Appender<Object>>();
+    int max = 10;
+    appenderTracker.setMaxComponents(max);
+    for (int i = 0; i < (max + 1); i++) {
+      Appender a = appenderTracker.getOrCreate(key + "-" + i, now++);
+      appenderList.add(a);
+    }
+    // cleaning only happens in removeStaleComponents
+    appenderTracker.removeStaleComponents(now++);
+    assertEquals(max, appenderTracker.allKeys().size());
+    assertNull(appenderTracker.find(key + "-" + 0));
+    assertFalse(appenderList.get(0).isStarted());
+  }
+
+  @Test
+  public void trackerShouldHonorTimeoutParameter() {
+    List<Appender<Object>> appenderList = new ArrayList<Appender<Object>>();
+    int timeout = 2;
+    appenderTracker.setTimeout(timeout);
+    for (int i = 0; i <= timeout; i++) {
+      Appender a = appenderTracker.getOrCreate(key + "-" + i, now++);
+      appenderList.add(a);
+    }
+
+    long numComponentsCreated = timeout + 1;
+    assertEquals(numComponentsCreated, appenderTracker.allKeys().size());
+
+    // cleaning only happens in removeStaleComponents. The first appender should timeout
+    appenderTracker.removeStaleComponents(now++);
+
+    // the first appender should have been removed
+    assertEquals(numComponentsCreated - 1, appenderTracker.allKeys().size());
+    assertNull(appenderTracker.find(key + "-" + 0));
+    assertFalse(appenderList.get(0).isStarted());
+
+    // the other appenders should be in the tracker
+    for (int i = 1; i <= timeout; i++) {
+      assertNotNull(appenderTracker.find(key + "-" + i));
+      assertTrue(appenderList.get(i).isStarted());
+    }
+  }
+  // ======================================================================
+  static class ListAppenderFactory implements AppenderFactory<Object> {
+
+    public Appender<Object> buildAppender(Context context, String discriminatingValue) throws JoranException {
+      ListAppender<Object> la = new ListAppender<Object>();
+      la.setContext(context);
+      la.setName(discriminatingValue);
+      la.start();
+      return la;
+    }
+  }
 }
