738117c95edb89f918abe9ace735155e55da26dd
Ceki Gulcu <ceki@qos.ch>
2013-04-26 18:14:44 +0200

    fix LOGBACK-230

--- logback-core/src/test/java/ch/qos/logback/core/joran/action/IncludeActionTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/joran/action/IncludeActionTest.java
@@ -121,6 +121,7 @@ public class IncludeActionTest {
   public void optionalFile() throws JoranException {
     tc.doConfigure(TOP_OPTIONAL);
     verifyConfig(new String[] { "IA", "IB" });
+    StatusPrinter.print(context);
   }
 
   @Test
