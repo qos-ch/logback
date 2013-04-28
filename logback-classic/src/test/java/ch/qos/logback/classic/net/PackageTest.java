a4fcd7b91e7ca46aca243e9d7cddac6a2156bad7
Carl Harris <ceharris@vt.edu>
2013-04-28 09:35:08 -0400

    removed old SocketAppenderTest from classic and access
    These test classes were really testing SocketAppenderBase not
    SocketAppender, per se.  The new implementation of SocketAppenderBase is
    significantly different and will require a new unit test suite.

--- logback-classic/src/test/java/ch/qos/logback/classic/net/PackageTest.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/net/PackageTest.java
@@ -19,7 +19,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses( { SyslogAppenderTest.class, DilutedSMTPAppenderTest.class,
-    SocketAppenderTest.class, JMSQueueAppenderTest.class, JMSTopicAppenderTest.class,
+        JMSQueueAppenderTest.class, JMSTopicAppenderTest.class,
         SMTPAppender_GreenTest.class, SMTPAppender_SubethaSMTPTest.class,
         SocketReceiverTest.class, SSLSocketReceiverTest.class })
 public class PackageTest {
