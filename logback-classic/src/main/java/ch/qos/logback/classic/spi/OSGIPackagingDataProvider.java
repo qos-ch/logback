898f5e0cc2936481473e755fde93feb108e92ed9
Chetan Mehrotra <chetanm@adobe.com>
2013-09-10 11:12:50 +0530

    Adding null checks

--- logback-classic/src/main/java/ch/qos/logback/classic/spi/OSGIPackagingDataProvider.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/spi/OSGIPackagingDataProvider.java
@@ -15,6 +15,7 @@ package ch.qos.logback.classic.spi;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.Version;
 
 /**
  * OSGi aware implementation of ClassPackagingDataProvider
@@ -23,14 +24,22 @@ public class OSGIPackagingDataProvider implements ClassPackagingDataProvider {
   private final ClassPackagingDataProvider defaultProvider = new DefaultPackagingDataProvider();
 
   public String getImplementationVersion(Class type) {
+    if(type == null){
+      return defaultProvider.getImplementationVersion(type);
+    }
     Bundle b = FrameworkUtil.getBundle(type);
     if(b == null){
       return defaultProvider.getImplementationVersion(type);
     }
-    return b.getVersion().toString();
+    final Version version = b.getVersion();
+    return version == org.osgi.framework.Version.emptyVersion ? "na" : version.toString();
   }
 
   public String getCodeLocation(Class type) {
+    if(type == null){
+      return defaultProvider.getCodeLocation(type);
+    }
+
     Bundle b = FrameworkUtil.getBundle(type);
     if(b == null){
       return defaultProvider.getCodeLocation(type);
