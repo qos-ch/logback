dd1d42fac3c707d3d358d503b351e12865d94edc
Sebastian Gröbler <s.groebler@googlemail.com>
2014-06-26 17:37:33 +0800

    added additional unit tests for stopping socket appender task on interrupts

--- logback-core/src/test/java/ch/qos/logback/core/net/AbstractSocketAppenderTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/net/AbstractSocketAppenderTest.java
@@ -31,7 +31,6 @@ import org.junit.After;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InOrder;
 import static org.mockito.Matchers.any;
@@ -77,11 +76,10 @@ public class AbstractSocketAppenderTest {
   private InstrumentedSocketAppender appender = spy(new InstrumentedSocketAppender(preSerializationTransformer, queueFactory, objectWriterFactory, socketConnector));
 
   @Before
-  public void setUp() throws Exception {
-    // setup valid appender with mock dependencies
-    when(socketConnector.call()).thenReturn(socket);
-    when(objectWriterFactory.newAutoFlushingObjectWriter(any(OutputStream.class))).thenReturn(objectWriter);
-    when(queueFactory.<String>newLinkedBlockingDeque(anyInt())).thenReturn(deque);
+  public void setupValidAppenderWithMockDependencies() throws Exception {
+    doReturn(socket).when(socketConnector).call();
+    doReturn(objectWriter).when(objectWriterFactory).newAutoFlushingObjectWriter(any(OutputStream.class));
+    doReturn(deque).when(queueFactory).<String>newLinkedBlockingDeque(anyInt());
 
     appender.setContext(mockContext);
     appender.setRemoteHost("localhost");
@@ -243,6 +241,45 @@ public class AbstractSocketAppenderTest {
 
     // then
     verify(appender, timeout(TIMEOUT).atLeastOnce()).addInfo(contains("connection closed"));
+  }
+
+  @Test
+  public void shutsDownOnInterruptWhileWaitingForEvent() throws Exception {
+
+    // given
+    doThrow(new InterruptedException()).when(deque).takeFirst();
+
+    // when
+    appender.start();
+
+    // then
+    verify(deque, timeout(TIMEOUT)).takeFirst();
+  }
+
+  @Test
+  public void shutsDownOnInterruptWhileWaitingForSocketConnection() throws Exception {
+
+    // given
+    doThrow(new InterruptedException()).when(socketConnector).call();
+
+    // when
+    appender.start();
+
+    // then
+    verify(socketConnector, timeout(TIMEOUT)).call();
+  }
+
+  @Test
+  public void addsInfoMessageWhenShuttingDownDueToInterrupt() throws Exception {
+
+    // given
+    doThrow(new InterruptedException()).when(socketConnector).call();
+
+    // when
+    appender.start();
+
+    // then
+    verify(appender, timeout(TIMEOUT)).addInfo(contains("shutting down"));
   }
 
   @Test
@@ -438,7 +475,7 @@ public class AbstractSocketAppenderTest {
   }
 
   private void breakOutSocketReConnectionLoop() throws InterruptedException {
-    when(socketConnector.call()).thenReturn(null);
+    doReturn(null).when(socketConnector).call();
   }
 
   private static class InstrumentedSocketAppender extends AbstractSocketAppender<String> {
