753d6d0f3f7b807505c8887cfda733788dc5ca39
KwonNam Son <kwon37xi@gmail.com>
2014-12-25 19:13:04 +0900

    LOGBACK-1008 ConfigurationDelegate.groovy should check AppenderAttachable.

--- logback-classic/src/main/groovy/ch/qos/logback/classic/gaffer/AppenderDelegate.groovy
+++ logback-classic/src/main/groovy/ch/qos/logback/classic/gaffer/AppenderDelegate.groovy
@@ -12,18 +12,37 @@
  * as published by the Free Software Foundation.
  */
 package ch.qos.logback.classic.gaffer
+import java.util.List;
+import java.util.Map;
+
 import ch.qos.logback.core.Appender
+import ch.qos.logback.core.spi.AppenderAttachable;
+
 /**
  * @author Ceki G&uuml;c&uuml;
  */
 class AppenderDelegate extends ComponentDelegate {
 
+  Map<String, Appender<?>> appendersByName = [:]
+
   AppenderDelegate(Appender appender) {
     super(appender)
   }
 
-  String getLabel() {
-    "appender"  
+  AppenderDelegate(Appender appender, List<Appender<?>> appenders) {
+    super(appender)
+    appendersByName = appenders.collectEntries { [(it.name) : it]}
   }
 
+  String getLabel() {
+    "appender"
+  }
+
+  void appenderRef(String name){
+    if (!AppenderAttachable.class.isAssignableFrom(component.class)) {
+        def errorMessage= component.class.name + ' does not implement ' + AppenderAttachable.class.name + '.'
+        throw new IllegalArgumentException(errorMessage)
+    }
+    component.addAppender(appendersByName[name])
+  }
 }
