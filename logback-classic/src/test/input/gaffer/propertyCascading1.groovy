24707be9f6ca0bc303a4f234b280c058fcb513ac
Tony Trinh <tony19@gmail.com>
2014-12-28 17:34:22 -0600

    Apply/update license file-header for all Groovy files

--- logback-classic/src/test/input/gaffer/propertyCascading1.groovy
+++ logback-classic/src/test/input/gaffer/propertyCascading1.groovy
@@ -1,3 +1,16 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
 //
 // Built on Wed May 19 20:51:44 CEST 2010 by logback-translator
 // For more information on configuration files in Groovy
