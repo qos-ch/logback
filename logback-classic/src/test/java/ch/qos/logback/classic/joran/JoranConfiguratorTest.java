31b87ab04d4d36bd85f137441f49c3ed8641c60b
Ceki Gulcu <ceki@qos.ch>
2016-02-23 21:15:20 +0100

    attempt to fix TravisCI failure

--- logback-classic/src/test/java/ch/qos/logback/classic/joran/JoranConfiguratorTest.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/joran/JoranConfiguratorTest.java
@@ -404,18 +404,16 @@ public class JoranConfiguratorTest {
   }
 
   @Test
-  public void levelChangePropagator1() throws JoranException, IOException,
-      InterruptedException {
+  public void levelChangePropagator1() throws JoranException, IOException,  InterruptedException {
     String loggerName = "changePropagator1" + diff;
-    java.util.logging.Logger.getLogger(loggerName).setLevel(
-        java.util.logging.Level.INFO);
+    java.util.logging.Logger logger1 = java.util.logging.Logger.getLogger(loggerName);
+    logger1.setLevel(java.util.logging.Level.INFO);
     verifyJULLevel(loggerName, Level.INFO);
-    String configFileAsStr = ClassicTestConstants.JORAN_INPUT_PREFIX
-        + "/jul/levelChangePropagator1.xml";
+    String configFileAsStr = ClassicTestConstants.JORAN_INPUT_PREFIX + "/jul/levelChangePropagator1.xml";
     configure(configFileAsStr);
     StatusChecker checker = new StatusChecker(loggerContext);
     checker.assertIsErrorFree();
-    verifyJULLevel(loggerName, Level.INFO);
+    verifyJULLevel(loggerName, Level.INFO); // 
     verifyJULLevel("a.b.c." + diff, Level.WARN);
     verifyJULLevel(Logger.ROOT_LOGGER_NAME, Level.TRACE);
   }
