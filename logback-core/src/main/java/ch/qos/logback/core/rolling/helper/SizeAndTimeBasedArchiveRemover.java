cb9cefe85f16e8ce935fe16a94060ef0d1fc2678
Ceki Gulcu <ceki@qos.ch>
2016-03-01 16:17:02 +0100

    revert to 2 space indentation to favilitate PR merge

--- logback-core/src/main/java/ch/qos/logback/core/rolling/helper/SizeAndTimeBasedArchiveRemover.java
+++ logback-core/src/main/java/ch/qos/logback/core/rolling/helper/SizeAndTimeBasedArchiveRemover.java
@@ -18,35 +18,39 @@ import java.util.Date;
 
 public class SizeAndTimeBasedArchiveRemover extends DefaultArchiveRemover {
 
-    public SizeAndTimeBasedArchiveRemover(FileNamePattern fileNamePattern, RollingCalendar rc) {
-        super(fileNamePattern, rc);
+  public SizeAndTimeBasedArchiveRemover(FileNamePattern fileNamePattern,
+          RollingCalendar rc) {
+    super(fileNamePattern, rc);
+  }
+
+  public void cleanByPeriodOffset(Date now, int periodOffset) {
+    Date dateOfPeriodToClean = rc.getEndOfNextNthPeriod(now, periodOffset);
+
+    String regex = fileNamePattern.toRegexForFixedDate(dateOfPeriodToClean);
+    String stemRegex = FileFilterUtil.afterLastSlash(regex);
+    File archive0 = new File(fileNamePattern.convertMultipleArguments(
+            dateOfPeriodToClean, 0));
+    // in case the file has no directory part, i.e. if it's written into the
+    // user's current directory.
+    archive0 = archive0.getAbsoluteFile();
+
+    File parentDir = archive0.getAbsoluteFile().getParentFile();
+    File[] matchingFileArray = FileFilterUtil.filesInFolderMatchingStemRegex(
+            parentDir, stemRegex);
+
+    for (File f : matchingFileArray) {
+      Date fileLastModified = rc.getEndOfNextNthPeriod(
+              new Date(f.lastModified()), -1);
+
+      if (fileLastModified.compareTo(dateOfPeriodToClean) <= 0) {
+        addInfo("deleting " + f);
+        f.delete();
+      }
     }
 
-    public void cleanByPeriodOffset(Date now, int periodOffset) {
-        Date dateOfPeriodToClean = rc.getEndOfNextNthPeriod(now, periodOffset);
-
-        String regex = fileNamePattern.toRegexForFixedDate(dateOfPeriodToClean);
-        String stemRegex = FileFilterUtil.afterLastSlash(regex);
-        File archive0 = new File(fileNamePattern.convertMultipleArguments(dateOfPeriodToClean, 0));
-        // in case the file has no directory part, i.e. if it's written into the
-        // user's current directory.
-        archive0 = archive0.getAbsoluteFile();
-
-        File parentDir = archive0.getAbsoluteFile().getParentFile();
-        File[] matchingFileArray = FileFilterUtil.filesInFolderMatchingStemRegex(parentDir, stemRegex);
-
-        for (File f : matchingFileArray) {
-            Date fileLastModified = rc.getEndOfNextNthPeriod(new Date(f.lastModified()), -1);
-
-            if (fileLastModified.compareTo(dateOfPeriodToClean) <= 0) {
-                addInfo("deleting " + f);
-                f.delete();
-            }
-        }
-
-        if (parentClean) {
-            removeFolderIfEmpty(parentDir);
-        }
+    if (parentClean) {
+      removeFolderIfEmpty(parentDir);
     }
+  }
 
 }
--- logback-core/src/test/java/ch/qos/logback/core/rolling/SizeAndTimeBasedFNATP_Test.java
+++ logback-core/src/test/java/ch/qos/logback/core/rolling/SizeAndTimeBasedFNATP_Test.java
@@ -30,196 +30,219 @@ import ch.qos.logback.core.status.StatusChecker;
 import ch.qos.logback.core.status.StatusManager;
 
 public class SizeAndTimeBasedFNATP_Test extends ScaffoldingForRollingTests {
-    private SizeAndTimeBasedFNATP<Object> sizeAndTimeBasedFNATP = null;
-    private RollingFileAppender<Object> rfa1 = new RollingFileAppender<Object>();
-    private TimeBasedRollingPolicy<Object> tbrp1 = new TimeBasedRollingPolicy<Object>();
-    private RollingFileAppender<Object> rfa2 = new RollingFileAppender<Object>();
-    private TimeBasedRollingPolicy<Object> tbrp2 = new TimeBasedRollingPolicy<Object>();
+  private SizeAndTimeBasedFNATP<Object> sizeAndTimeBasedFNATP = null;
+  private RollingFileAppender<Object> rfa1 = new RollingFileAppender<Object>();
+  private TimeBasedRollingPolicy<Object> tbrp1 = new TimeBasedRollingPolicy<Object>();
+  private RollingFileAppender<Object> rfa2 = new RollingFileAppender<Object>();
+  private TimeBasedRollingPolicy<Object> tbrp2 = new TimeBasedRollingPolicy<Object>();
 
-    private EchoEncoder<Object> encoder = new EchoEncoder<Object>();
-    int fileSize = 0;
-    int fileIndexCounter = 0;
-    int sizeThreshold = 0;
+  private EchoEncoder<Object> encoder = new EchoEncoder<Object>();
+  int fileSize = 0;
+  int fileIndexCounter = 0;
+  int sizeThreshold = 0;
 
-    @Before
-    public void setUp() {
-        super.setUp();
+  @Before
+  public void setUp() {
+    super.setUp();
+  }
+
+  private void initRollingFileAppender(RollingFileAppender<Object> rfa,
+          String filename) {
+    rfa.setContext(context);
+    rfa.setEncoder(encoder);
+    if (filename != null) {
+      rfa.setFile(filename);
+    }
+  }
+
+  private void initPolicies(RollingFileAppender<Object> rfa,
+          TimeBasedRollingPolicy<Object> tbrp, String filenamePattern,
+          int sizeThreshold, long givenTime, long lastCheck) {
+    sizeAndTimeBasedFNATP = new SizeAndTimeBasedFNATP<Object>();
+    tbrp.setContext(context);
+    sizeAndTimeBasedFNATP.setMaxFileSize("" + sizeThreshold);
+    tbrp.setTimeBasedFileNamingAndTriggeringPolicy(sizeAndTimeBasedFNATP);
+    tbrp.setFileNamePattern(filenamePattern);
+    tbrp.setParent(rfa);
+    tbrp.timeBasedFileNamingAndTriggeringPolicy.setCurrentTime(givenTime);
+    rfa.setRollingPolicy(tbrp);
+    tbrp.start();
+    rfa.start();
+  }
+
+  private void addExpectedFileNamedIfItsTime(String randomOutputDir,
+          String testId, String msg, String compressionSuffix) {
+    fileSize = fileSize + msg.getBytes().length;
+    if (passThresholdTime(nextRolloverThreshold)) {
+      fileIndexCounter = 0;
+      fileSize = 0;
+      addExpectedFileName_ByFileIndexCounter(randomOutputDir, testId,
+              getMillisOfCurrentPeriodsStart(), fileIndexCounter,
+              compressionSuffix);
+      recomputeRolloverThreshold(currentTime);
+      return;
     }
 
-    private void initRollingFileAppender(RollingFileAppender<Object> rfa, String filename) {
-        rfa.setContext(context);
-        rfa.setEncoder(encoder);
-        if (filename != null) {
-            rfa.setFile(filename);
-        }
+    // windows can delay file size changes, so we only allow for
+    // fileIndexCounter 0
+    if ((fileIndexCounter < 1) && fileSize > sizeThreshold) {
+      addExpectedFileName_ByFileIndexCounter(randomOutputDir, testId,
+              getMillisOfCurrentPeriodsStart(), fileIndexCounter,
+              compressionSuffix);
+      fileIndexCounter = fileIndexCounter + 1;
+      fileSize = 0;
+    }
+  }
+
+  void generic(String testId, String stem, boolean withSecondPhase,
+          String compressionSuffix) throws IOException, InterruptedException,
+          ExecutionException {
+    String file = (stem != null) ? randomOutputDir + stem : null;
+    initRollingFileAppender(rfa1, file);
+    sizeThreshold = 300;
+    initPolicies(rfa1, tbrp1, randomOutputDir + testId + "-%d{"
+            + DATE_PATTERN_WITH_SECONDS + "}-%i.txt" + compressionSuffix,
+            sizeThreshold, currentTime, 0);
+    addExpectedFileName_ByFileIndexCounter(randomOutputDir, testId,
+            getMillisOfCurrentPeriodsStart(), fileIndexCounter,
+            compressionSuffix);
+    incCurrentTime(100);
+    tbrp1.timeBasedFileNamingAndTriggeringPolicy.setCurrentTime(currentTime);
+    int runLength = 100;
+    String prefix = "Hello -----------------";
+
+    for (int i = 0; i < runLength; i++) {
+      String msg = prefix + i;
+      rfa1.doAppend(msg);
+      addExpectedFileNamedIfItsTime(randomOutputDir, testId, msg,
+              compressionSuffix);
+      incCurrentTime(20);
+      tbrp1.timeBasedFileNamingAndTriggeringPolicy.setCurrentTime(currentTime);
+      add(tbrp1.future);
     }
 
-    private void initPolicies(RollingFileAppender<Object> rfa, TimeBasedRollingPolicy<Object> tbrp, String filenamePattern, int sizeThreshold, long givenTime,
-                    long lastCheck) {
-        sizeAndTimeBasedFNATP = new SizeAndTimeBasedFNATP<Object>();
-        tbrp.setContext(context);
-        sizeAndTimeBasedFNATP.setMaxFileSize("" + sizeThreshold);
-        tbrp.setTimeBasedFileNamingAndTriggeringPolicy(sizeAndTimeBasedFNATP);
-        tbrp.setFileNamePattern(filenamePattern);
-        tbrp.setParent(rfa);
-        tbrp.timeBasedFileNamingAndTriggeringPolicy.setCurrentTime(givenTime);
-        rfa.setRollingPolicy(tbrp);
-        tbrp.start();
-        rfa.start();
+    if (withSecondPhase) {
+      secondPhase(testId, file, stem, compressionSuffix, runLength, prefix);
+      runLength = runLength * 2;
     }
 
-    private void addExpectedFileNamedIfItsTime(String randomOutputDir, String testId, String msg, String compressionSuffix) {
-        fileSize = fileSize + msg.getBytes().length;
-        if (passThresholdTime(nextRolloverThreshold)) {
-            fileIndexCounter = 0;
-            fileSize = 0;
-            addExpectedFileName_ByFileIndexCounter(randomOutputDir, testId, getMillisOfCurrentPeriodsStart(), fileIndexCounter, compressionSuffix);
-            recomputeRolloverThreshold(currentTime);
-            return;
-        }
+    if (stem != null)
+      massageExpectedFilesToCorresponToCurrentTarget(file, true);
 
-        // windows can delay file size changes, so we only allow for
-        // fileIndexCounter 0
-        if ((fileIndexCounter < 1) && fileSize > sizeThreshold) {
-            addExpectedFileName_ByFileIndexCounter(randomOutputDir, testId, getMillisOfCurrentPeriodsStart(), fileIndexCounter, compressionSuffix);
-            fileIndexCounter = fileIndexCounter + 1;
-            fileSize = 0;
-        }
+    Thread.yield();
+    // wait for compression to finish
+    waitForJobsToComplete();
+
+    // StatusPrinter.print(context);
+    existenceCheck(expectedFilenameList);
+    sortedContentCheck(randomOutputDir, runLength, prefix);
+  }
+
+  void secondPhase(String testId, String file, String stem,
+          String compressionSuffix, int runLength, String prefix) {
+    rfa1.stop();
+
+    if (stem != null) {
+      File f = new File(file);
+      f.setLastModified(currentTime);
     }
 
-    void generic(String testId, String stem, boolean withSecondPhase, String compressionSuffix) throws IOException, InterruptedException, ExecutionException {
-        String file = (stem != null) ? randomOutputDir + stem : null;
-        initRollingFileAppender(rfa1, file);
-        sizeThreshold = 300;
-        initPolicies(rfa1, tbrp1, randomOutputDir + testId + "-%d{" + DATE_PATTERN_WITH_SECONDS + "}-%i.txt" + compressionSuffix, sizeThreshold, currentTime, 0);
-        addExpectedFileName_ByFileIndexCounter(randomOutputDir, testId, getMillisOfCurrentPeriodsStart(), fileIndexCounter, compressionSuffix);
-        incCurrentTime(100);
-        tbrp1.timeBasedFileNamingAndTriggeringPolicy.setCurrentTime(currentTime);
-        int runLength = 100;
-        String prefix = "Hello -----------------";
+    StatusManager sm = context.getStatusManager();
+    sm.add(new InfoStatus(
+            "Time when rfa1 is stopped: " + new Date(currentTime), this));
+    sm.add(new InfoStatus("currentTime%1000=" + (currentTime % 1000), this));
 
-        for (int i = 0; i < runLength; i++) {
-            String msg = prefix + i;
-            rfa1.doAppend(msg);
-            addExpectedFileNamedIfItsTime(randomOutputDir, testId, msg, compressionSuffix);
-            incCurrentTime(20);
-            tbrp1.timeBasedFileNamingAndTriggeringPolicy.setCurrentTime(currentTime);
-            add(tbrp1.future);
-        }
+    initRollingFileAppender(rfa2, file);
+    initPolicies(rfa2, tbrp2, randomOutputDir + testId + "-%d{"
+            + DATE_PATTERN_WITH_SECONDS + "}-%i.txt" + compressionSuffix,
+            sizeThreshold, currentTime, 0);
 
-        if (withSecondPhase) {
-            secondPhase(testId, file, stem, compressionSuffix, runLength, prefix);
-            runLength = runLength * 2;
-        }
-
-        if (stem != null)
-            massageExpectedFilesToCorresponToCurrentTarget(file, true);
-
-        Thread.yield();
-        // wait for compression to finish
-        waitForJobsToComplete();
-
-        // StatusPrinter.print(context);
-        existenceCheck(expectedFilenameList);
-        sortedContentCheck(randomOutputDir, runLength, prefix);
+    for (int i = runLength; i < runLength * 2; i++) {
+      incCurrentTime(100);
+      tbrp2.timeBasedFileNamingAndTriggeringPolicy.setCurrentTime(currentTime);
+      String msg = prefix + i;
+      rfa2.doAppend(msg);
+      addExpectedFileNamedIfItsTime(randomOutputDir, testId, msg,
+              compressionSuffix);
     }
+  }
 
-    void secondPhase(String testId, String file, String stem, String compressionSuffix, int runLength, String prefix) {
-        rfa1.stop();
+  static final boolean FIRST_PHASE_ONLY = false;
+  static final boolean WITH_SECOND_PHASE = true;
+  static String DEFAULT_COMPRESSION_SUFFIX = "";
 
-        if (stem != null) {
-            File f = new File(file);
-            f.setLastModified(currentTime);
-        }
+  @Test
+  public void noCompression_FileSet_NoRestart_1() throws InterruptedException,
+          ExecutionException, IOException {
+    generic("test1", "toto.log", FIRST_PHASE_ONLY, DEFAULT_COMPRESSION_SUFFIX);
+  }
 
-        StatusManager sm = context.getStatusManager();
-        sm.add(new InfoStatus("Time when rfa1 is stopped: " + new Date(currentTime), this));
-        sm.add(new InfoStatus("currentTime%1000=" + (currentTime % 1000), this));
+  @Test
+  public void noCompression_FileBlank_NoRestart_2() throws Exception {
+    generic("test2", null, FIRST_PHASE_ONLY, DEFAULT_COMPRESSION_SUFFIX);
+  }
 
-        initRollingFileAppender(rfa2, file);
-        initPolicies(rfa2, tbrp2, randomOutputDir + testId + "-%d{" + DATE_PATTERN_WITH_SECONDS + "}-%i.txt" + compressionSuffix, sizeThreshold, currentTime, 0);
+  @Test
+  public void noCompression_FileBlank_WithStopStart_3() throws Exception {
+    generic("test3", null, WITH_SECOND_PHASE, DEFAULT_COMPRESSION_SUFFIX);
+  }
 
-        for (int i = runLength; i < runLength * 2; i++) {
-            incCurrentTime(100);
-            tbrp2.timeBasedFileNamingAndTriggeringPolicy.setCurrentTime(currentTime);
-            String msg = prefix + i;
-            rfa2.doAppend(msg);
-            addExpectedFileNamedIfItsTime(randomOutputDir, testId, msg, compressionSuffix);
-        }
-    }
+  @Test
+  public void noCompression_FileSet_WithStopStart_4() throws Exception {
+    generic("test4", "test4.log", WITH_SECOND_PHASE, DEFAULT_COMPRESSION_SUFFIX);
+  }
 
-    static final boolean FIRST_PHASE_ONLY = false;
-    static final boolean WITH_SECOND_PHASE = true;
-    static String DEFAULT_COMPRESSION_SUFFIX = "";
+  @Test
+  public void withGZCompression_FileSet_NoRestart_5() throws Exception {
+    generic("test5", "toto.log", FIRST_PHASE_ONLY, ".gz");
+  }
 
-    @Test
-    public void noCompression_FileSet_NoRestart_1() throws InterruptedException, ExecutionException, IOException {
-        generic("test1", "toto.log", FIRST_PHASE_ONLY, DEFAULT_COMPRESSION_SUFFIX);
-    }
+  @Test
+  public void withGZCompression_FileBlank_NoRestart_6() throws Exception {
+    generic("test6", null, FIRST_PHASE_ONLY, ".gz");
+  }
 
-    @Test
-    public void noCompression_FileBlank_NoRestart_2() throws Exception {
-        generic("test2", null, FIRST_PHASE_ONLY, DEFAULT_COMPRESSION_SUFFIX);
-    }
+  @Test
+  public void withZipCompression_FileSet_NoRestart_7() throws Exception {
+    generic("test7", "toto.log", FIRST_PHASE_ONLY, ".zip");
+    List<String> zipFiles = filterElementsInListBySuffix(".zip");
+    checkZipEntryMatchesZipFilename(zipFiles);
+  }
 
-    @Test
-    public void noCompression_FileBlank_WithStopStart_3() throws Exception {
-        generic("test3", null, WITH_SECOND_PHASE, DEFAULT_COMPRESSION_SUFFIX);
-    }
+  @Test
+  public void checkMissingIntToken() {
+    String stem = "toto.log";
+    String testId = "checkMissingIntToken";
+    String compressionSuffix = "gz";
 
-    @Test
-    public void noCompression_FileSet_WithStopStart_4() throws Exception {
-        generic("test4", "test4.log", WITH_SECOND_PHASE, DEFAULT_COMPRESSION_SUFFIX);
-    }
+    String file = (stem != null) ? randomOutputDir + stem : null;
+    initRollingFileAppender(rfa1, file);
+    sizeThreshold = 300;
+    initPolicies(rfa1, tbrp1, randomOutputDir + testId + "-%d{"
+            + DATE_PATTERN_WITH_SECONDS + "}.txt" + compressionSuffix,
+            sizeThreshold, currentTime, 0);
 
-    @Test
-    public void withGZCompression_FileSet_NoRestart_5() throws Exception {
-        generic("test5", "toto.log", FIRST_PHASE_ONLY, ".gz");
-    }
+    // StatusPrinter.print(context);
+    assertFalse(rfa1.isStarted());
+    StatusChecker checker = new StatusChecker(context);
+    checker.assertContainsMatch("Missing integer token");
+  }
 
-    @Test
-    public void withGZCompression_FileBlank_NoRestart_6() throws Exception {
-        generic("test6", null, FIRST_PHASE_ONLY, ".gz");
-    }
+  @Test
+  public void checkDateCollision() {
+    String stem = "toto.log";
+    String testId = "checkDateCollision";
+    String compressionSuffix = "gz";
 
-    @Test
-    public void withZipCompression_FileSet_NoRestart_7() throws Exception {
-        generic("test7", "toto.log", FIRST_PHASE_ONLY, ".zip");
-        List<String> zipFiles = filterElementsInListBySuffix(".zip");
-        checkZipEntryMatchesZipFilename(zipFiles);
-    }
+    String file = (stem != null) ? randomOutputDir + stem : null;
+    initRollingFileAppender(rfa1, file);
+    sizeThreshold = 300;
+    initPolicies(rfa1, tbrp1, randomOutputDir + testId + "-%d{EE}.txt"
+            + compressionSuffix, sizeThreshold, currentTime, 0);
 
-    @Test
-    public void checkMissingIntToken() {
-        String stem = "toto.log";
-        String testId = "checkMissingIntToken";
-        String compressionSuffix = "gz";
-
-        String file = (stem != null) ? randomOutputDir + stem : null;
-        initRollingFileAppender(rfa1, file);
-        sizeThreshold = 300;
-        initPolicies(rfa1, tbrp1, randomOutputDir + testId + "-%d{" + DATE_PATTERN_WITH_SECONDS + "}.txt" + compressionSuffix, sizeThreshold, currentTime, 0);
-
-        // StatusPrinter.print(context);
-        assertFalse(rfa1.isStarted());
-        StatusChecker checker = new StatusChecker(context);
-        checker.assertContainsMatch("Missing integer token");
-    }
-
-    @Test
-    public void checkDateCollision() {
-        String stem = "toto.log";
-        String testId = "checkDateCollision";
-        String compressionSuffix = "gz";
-
-        String file = (stem != null) ? randomOutputDir + stem : null;
-        initRollingFileAppender(rfa1, file);
-        sizeThreshold = 300;
-        initPolicies(rfa1, tbrp1, randomOutputDir + testId + "-%d{EE}.txt" + compressionSuffix, sizeThreshold, currentTime, 0);
-
-        // StatusPrinter.print(context);
-        assertFalse(rfa1.isStarted());
-        StatusChecker checker = new StatusChecker(context);
-        checker.assertContainsMatch("The date format in FileNamePattern");
-    }
+    // StatusPrinter.print(context);
+    assertFalse(rfa1.isStarted());
+    StatusChecker checker = new StatusChecker(context);
+    checker.assertContainsMatch("The date format in FileNamePattern");
+  }
 }
