6dffdfaa981471ad4ca924fd2283d7630b6594df
Ceki Gulcu <ceki@qos.ch>
2016-03-03 17:54:09 +0100

    make test more reobust

--- logback-classic/src/test/java/ch/qos/logback/classic/pattern/ConverterTest.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/pattern/ConverterTest.java
@@ -325,8 +325,11 @@ public class ConverterTest {
         DynamicConverter<ILoggingEvent> converter = new RelativeTimeConverter();
         StringBuilder buf0 = new StringBuilder();
         StringBuilder buf1 = new StringBuilder();
-        ILoggingEvent e0 = makeLoggingEvent(null);
-        ILoggingEvent e1 = makeLoggingEvent(null);
+        long timestamp = System.currentTimeMillis();
+        LoggingEvent e0 = makeLoggingEvent(null);
+        e0.setTimeStamp(timestamp);
+        LoggingEvent e1 = makeLoggingEvent(null);
+        e0.setTimeStamp(timestamp);
         converter.write(buf0, e0);
         converter.write(buf1, e1);
         assertEquals(buf0.toString(), buf1.toString());
