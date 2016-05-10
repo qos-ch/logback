c02aa8a33e92f81b91cf586347a02bfe0068f594
Ceki Gulcu <ceki@qos.ch>
2016-05-10 12:17:18 +0200

    minor edit

--- logback-classic/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ logback-classic/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -91,8 +91,7 @@ public class StaticLoggerBinder implements LoggerFactoryBinder {
             }
             contextSelectorBinder.init(defaultLoggerContext, KEY);
             initialized = true;
-        } catch (Exception t) {//do not swallow Errors
-            // we should never get here
+        } catch (Exception t) { // see LOGBACK-1159
             Util.report("Failed to instantiate [" + LoggerContext.class.getName() + "]", t);
         }
     }
