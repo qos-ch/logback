7640ab534bdfceada914be9232dae45b3246773e
Garg, Raghvendra <Raghvendra.Garg@cdk.com>
2016-08-31 12:24:34 +0530

    fix LOGBACK-1067 (The SMTPAppender's cyclicBufferTracker bufferSize param does't work)

--- logback-core/src/main/java/ch/qos/logback/core/helpers/CyclicBuffer.java
+++ logback-core/src/main/java/ch/qos/logback/core/helpers/CyclicBuffer.java
@@ -18,10 +18,10 @@ import java.util.List;
 
 /**
  * CyclicBuffer holds values in a cyclic array.
- * 
+ *
  * <p>It allows read access to any element in the buffer not just the first or
  * last element.
- * 
+ *
  * @author Ceki G&uuml;lc&uuml;
  */
 public class CyclicBuffer<E> {
@@ -31,12 +31,14 @@ public class CyclicBuffer<E> {
     int last;
     int numElems;
     int maxSize;
+    // time stamp when the buffer was created or cleared
+    long bufferCreatedTimeStamp;
 
     /**
      * Instantiate a new CyclicBuffer of at most <code>maxSize</code> events.
-     * 
+     *
      * The <code>maxSize</code> argument must a positive integer.
-     * 
+     *
      * @param maxSize
      *                The maximum number of elements in the buffer.
      */
@@ -54,6 +56,7 @@ public class CyclicBuffer<E> {
         this.last = other.last;
         this.first = other.first;
         this.numElems = other.numElems;
+        this.bufferCreatedTimeStamp = other.bufferCreatedTimeStamp;
     }
 
     @SuppressWarnings("unchecked")
@@ -63,6 +66,7 @@ public class CyclicBuffer<E> {
         first = 0;
         last = 0;
         numElems = 0;
+        this.bufferCreatedTimeStamp = System.currentTimeMillis();
     }
 
     /**
@@ -74,7 +78,7 @@ public class CyclicBuffer<E> {
 
     /**
      * Add an <code>event</code> as the last event in the buffer.
-     * 
+     *
      */
     public void add(E event) {
         ea[last] = event;
@@ -137,7 +141,7 @@ public class CyclicBuffer<E> {
 
     /**
      * Resize the cyclic buffer to <code>newSize</code>.
-     * 
+     *
      * @throws IllegalArgumentException
      *                 if <code>newSize</code> is negative.
      */
@@ -169,5 +173,9 @@ public class CyclicBuffer<E> {
         } else {
             last = loopLen;
         }
+    }
+
+    public long getBufferCreatedTimeStamp() {
+        return bufferCreatedTimeStamp;
     }
 }
