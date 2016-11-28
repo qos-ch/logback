f3fa960a989ac2916209ac08c000e6ae0b7429e8
kurt <kurt@kurt.pe.kr>
2016-11-28 11:26:23 +0900

    correct the indentation.

--- logback-access/src/main/java/ch/qos/logback/access/net/SocketAppender.java
+++ logback-access/src/main/java/ch/qos/logback/access/net/SocketAppender.java
@@ -37,11 +37,11 @@ public class SocketAppender extends AbstractSocketAppender<IAccessEvent> {
     public SocketAppender() {
     }
 
-	@Override
-	protected void append(IAccessEvent event) {
-		event.prepareForDeferredProcessing();
-		super.append(event);
-	}
+    @Override
+    protected void append(IAccessEvent event) {
+        event.prepareForDeferredProcessing();
+        super.append(event);
+    }
 
     public PreSerializationTransformer<IAccessEvent> getPST() {
         return pst;
