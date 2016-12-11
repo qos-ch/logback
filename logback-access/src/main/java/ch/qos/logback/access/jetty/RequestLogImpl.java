6a88a76f69e43b0c43bb5561aee639ccaba47bff
Ceki Gulcu <ceki@qos.ch>
2016-12-11 21:56:20 +0100

    minor edits

--- logback-access/src/main/java/ch/qos/logback/access/jetty/RequestLogImpl.java
+++ logback-access/src/main/java/ch/qos/logback/access/jetty/RequestLogImpl.java
@@ -18,7 +18,6 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import ch.qos.logback.core.status.InfoStatus;
 import ch.qos.logback.core.util.FileUtil;
