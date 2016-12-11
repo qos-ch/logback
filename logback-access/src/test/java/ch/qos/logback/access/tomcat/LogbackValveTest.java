006e8408f6c88d8875a74f3895c94d3aaeb76e36
Ceki Gulcu <ceki@qos.ch>
2016-12-11 21:31:37 +0100

    fix LOGBACLK-1181

--- logback-access/src/test/java/ch/qos/logback/access/tomcat/LogbackValveTest.java
+++ logback-access/src/test/java/ch/qos/logback/access/tomcat/LogbackValveTest.java
@@ -13,6 +13,8 @@
  */
 package ch.qos.logback.access.tomcat;
 
+import static org.junit.Assert.*;
+
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.core.ContainerBase;
 import org.junit.After;
@@ -73,6 +75,15 @@ public class LogbackValveTest {
         checker.assertIsErrorFree();
     }
 
+    @Test
+    public void executorServiceShouldBeNotNull() throws LifecycleException {
+        final String fileName = "logback-asResource.xml";
+        setupValve(fileName);
+        valve.start();
+        assertNotNull(valve.getScheduledExecutorService());
+        
+    }
+    
     private void setupValve(final String resourceName) {
         valve.setFilename(resourceName);
         valve.setName("test");
