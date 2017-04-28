3dbda6a6b9e935f5dbb4157f760154c242bf41ad
Ceki Gulcu <ceki@qos.ch>
2017-04-28 19:14:08 +0200

    almost compiles

--- logback-classic/src/main/java/module-info.java
+++ logback-classic/src/main/java/module-info.java
@@ -2,10 +2,15 @@ module ch.qos.logback.classic {
 
 	requires org.slf4j;
 	requires ch.qos.logback.core;
-//	requires java.naming;
-//	requires static java.sql;
-//	requires static mail;
-//	requires static janino;
-//	requires static javax.servlet.api;
-//	requires static commons.compiler;
+	
+	requires java.xml;
+	requires java.management;
+	requires java.naming;
+	requires static java.sql;
+	requires static mail;
+	requires static janino;
+	requires static javax.servlet.api;
+	requires static commons.compiler;
+	requires static groovy.all;
+	
 }
