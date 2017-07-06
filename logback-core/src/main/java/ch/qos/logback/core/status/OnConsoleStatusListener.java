c772abc1b080ed59e55e76dd60d3993e0721cc0e
Tillmann Heigel <tillmann.heigel@idealo.de>
2017-07-06 09:38:10 +0200

    CustomStatusLevelStatusListener: Set StatusLevel for OnConsoleStatusListener in logback.xml

--- logback-core/src/main/java/ch/qos/logback/core/status/OnConsoleStatusListener.java
+++ logback-core/src/main/java/ch/qos/logback/core/status/OnConsoleStatusListener.java
@@ -15,17 +15,64 @@ package ch.qos.logback.core.status;
 
 import java.io.PrintStream;
 
+import ch.qos.logback.core.util.StatusPrinter;
+
 /**
- * Print all new incoming status messages on the console (System.out).
+ * Print new incoming status messages on the console (System.out).
  *
- * @author Ceki G&uuml;lc&uuml;
+ * Optionally a custom LogLevel can be set:
+ *
+ * <pre>
+ * {@code
+ * <statusListener class="ch.qos.logback.core.status.OnConsoleStatusListener">
+ *     <level>WARN</level>
+ * </statusListener>
+ * }
+ * </pre>
+ *
+ * @author Ceki G&uuml;lc&uuml;, Tillmann Heigel
  */
 public class OnConsoleStatusListener extends OnPrintStreamStatusListenerBase {
-    
-    
-    @Override
+
+    private int statusLevel = Status.INFO;
+
+    private void print(Status status) {
+        StringBuilder sb = new StringBuilder();
+
+        if (prefix != null) {
+            sb.append(prefix);
+        }
+
+        if (status.getLevel() >= statusLevel) {
+            StatusPrinter.buildStr(sb, "", status);
+            getPrintStream().print(sb);
+        }
+    }
+
     protected PrintStream getPrintStream() {
         return System.out;
     }
 
+    public void addStatusEvent(Status status) {
+        if (!isStarted) {
+            return;
+        }
+        print(status);
+    }
+
+    public void setLevel(String level) {
+        switch (level) {
+            case "INFO":
+                statusLevel = Status.INFO;
+                break;
+            case "WARN":
+                statusLevel = Status.WARN;
+                break;
+            case "ERROR":
+                statusLevel = Status.ERROR;
+                break;
+            default:
+                statusLevel = Status.INFO;
+        }
+    }
 }
