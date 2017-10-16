f9f9dfd81f194ab3df45f3c507664cbb0da7d9f2
Ceki Gulcu <ceki@qos.ch>
2017-10-16 22:43:24 +0200

    LOGBACK-1260 improvements

--- logback-core/src/main/java/ch/qos/logback/core/util/EnvUtil.java
+++ logback-core/src/main/java/ch/qos/logback/core/util/EnvUtil.java
@@ -18,47 +18,58 @@ package ch.qos.logback.core.util;
  */
 public class EnvUtil {
 
-    private EnvUtil() {}
+	private EnvUtil() {
+	}
 
-    static private boolean isJDK_N_OrHigher(int n) {
-        int version = 0;
-        for (byte ch : System.getProperty("java.version", "").getBytes()) {
-            if (Character.isDigit(ch)) {
-                version = (version * 10) + (ch - 48);
-            } else if (version == 1) {
-                version = 0;
-            } else {
-                break;
-            }
-        }
-        return version > 0 && n <= version;
-    }
+	static public int getJDKVersion(String javaVersionStr) {
+		int version = 0;
+		
+		for (char ch : javaVersionStr.toCharArray()) {
+			if (Character.isDigit(ch)) {
+				version = (version * 10) + (ch - 48);
+			} else if (version == 1) {
+				version = 0;
+			} else {
+				break;
+			}
+		}
+		return version;
+	}
 
-    static public boolean isJDK5() {
-        return isJDK_N_OrHigher(5);
-    }
+	static private boolean isJDK_N_OrHigher(int n) {
+		String javaVersionStr = System.getProperty("java.version", "");
+		if (javaVersionStr.isEmpty())
+			return false;
 
-    static public boolean isJDK6OrHigher() {
-        return isJDK_N_OrHigher(6);
-    }
+		int version = getJDKVersion(javaVersionStr);
+		return version > 0 && n <= version;
+	}
 
-    static public boolean isJDK7OrHigher() {
-        return isJDK_N_OrHigher(7);
-    }
+	static public boolean isJDK5() {
+		return isJDK_N_OrHigher(5);
+	}
 
-    static public boolean isJaninoAvailable() {
-        ClassLoader classLoader = EnvUtil.class.getClassLoader();
-        try {
-            Class<?> bindingClass = classLoader.loadClass("org.codehaus.janino.ScriptEvaluator");
-            return (bindingClass != null);
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
+	static public boolean isJDK6OrHigher() {
+		return isJDK_N_OrHigher(6);
+	}
 
-    public static boolean isWindows() {
-        String os = System.getProperty("os.name");
-        return os.startsWith("Windows");
-    }
+	static public boolean isJDK7OrHigher() {
+		return isJDK_N_OrHigher(7);
+	}
+
+	static public boolean isJaninoAvailable() {
+		ClassLoader classLoader = EnvUtil.class.getClassLoader();
+		try {
+			Class<?> bindingClass = classLoader.loadClass("org.codehaus.janino.ScriptEvaluator");
+			return (bindingClass != null);
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
+	}
+
+	public static boolean isWindows() {
+		String os = System.getProperty("os.name");
+		return os.startsWith("Windows");
+	}
 
 }
