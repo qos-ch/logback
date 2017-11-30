6faf5d375dd619857357fb71ca729f0b2514d6c4
Ceki Gulcu <ceki@qos.ch>
2017-11-30 22:58:25 +0100

    ongoing work on modularizing logback

--- logback-classic/src/main/java/ch/qos/logback/classic/provider/LogbackServiceProvider.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/provider/LogbackServiceProvider.java
@@ -2,11 +2,24 @@ package ch.qos.logback.classic.provider;
 
 import org.slf4j.ILoggerFactory;
 import org.slf4j.IMarkerFactory;
+import org.slf4j.helpers.BasicMarkerFactory;
+import org.slf4j.helpers.Util;
 import org.slf4j.spi.MDCAdapter;
 import org.slf4j.spi.SLF4JServiceProvider;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.classic.util.ContextSelectorStaticBinder;
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
+import ch.qos.logback.core.CoreConstants;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.status.StatusUtil;
+import ch.qos.logback.core.util.StatusPrinter;
+
 public class LogbackServiceProvider implements SLF4JServiceProvider {
-    
+
+    final static String NULL_CS_URL = CoreConstants.CODES_URL + "#null_CS";
+
     /**
      * Declare the version of the SLF4J API this implementation is compiled against. 
      * The value of this field is modified with each major release. 
@@ -14,34 +27,53 @@ public class LogbackServiceProvider implements SLF4JServiceProvider {
     // to avoid constant folding by the compiler, this field must *not* be final
     public static String REQUESTED_API_VERSION = "1.8.99"; // !final
 
-    
+    private LoggerContext loggerFactory;
+    private IMarkerFactory markerFactory;
+    private MDCAdapter mdcAdapter;
+
     @Override
+    public void initialize() {
+        loggerFactory = new LoggerContext();
+        initializeLoggerContext(loggerFactory);
+        markerFactory = new BasicMarkerFactory();
+        mdcAdapter = new LogbackMDCAdapter();
+    }
+
+    private void initializeLoggerContext(LoggerContext loggerFactory) {
+        try {
+            try {
+                new ContextInitializer(loggerFactory).autoConfig();
+            } catch (JoranException je) {
+                Util.report("Failed to auto configure default logger context", je);
+            }
+            // logback-292
+            if (!StatusUtil.contextHasStatusListener(loggerFactory)) {
+                StatusPrinter.printInCaseOfErrorsOrWarnings(loggerFactory);
+            }
+        } catch (Exception t) { // see LOGBACK-1159
+            Util.report("Failed to instantiate [" + LoggerContext.class.getName() + "]", t);
+        }
+    }
+
+    @Override
+
     public ILoggerFactory getLoggerFactory() {
-        // TODO Auto-generated method stub
-        return null;
+        return loggerFactory;
     }
 
     @Override
     public IMarkerFactory getMarkerFactory() {
-        // TODO Auto-generated method stub
-        return null;
+        return markerFactory;
     }
 
     @Override
     public MDCAdapter getMDCAdapter() {
-        // TODO Auto-generated method stub
-        return null;
+        return mdcAdapter;
     }
 
     @Override
     public String getRequesteApiVersion() {
         return REQUESTED_API_VERSION;
-    }
-
-    @Override
-    public void initialize() {
-        // TODO Auto-generated method stub
-        
     }
 
 }
