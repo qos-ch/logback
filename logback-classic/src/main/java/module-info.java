d530a6be5a30505dcc2ee100c76f21a92b522aba
Ceki Gulcu <ceki@qos.ch>
2017-12-03 00:17:50 +0100

    first working version with jigsaw modules

--- logback-classic/src/main/java/module-info.java
+++ logback-classic/src/main/java/module-info.java
@@ -4,10 +4,8 @@ module ch.qos.logback.classic {
     requires static java.logging;
     requires static java.management;
     
-    
-    
     requires ch.qos.logback.core;
-    requires static groovy;
+    //requires static groovy;
     
     provides org.slf4j.spi.SLF4JServiceProvider with ch.qos.logback.classic.spi.LogbackServiceProvider;
 
