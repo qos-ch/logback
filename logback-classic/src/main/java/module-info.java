c94277ab07e944358c4f3332d5e852f3b978a2f1
Ceki Gulcu <ceki@qos.ch>
2017-12-03 16:25:46 +0100

    sucessful jigsaw modularization - requires unoffical groovy.jar

--- logback-classic/src/main/java/module-info.java
+++ logback-classic/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module ch.qos.logback.classic {
     requires static java.management;
     
     requires ch.qos.logback.core;
-    //requires static groovy;
+    requires static groovy;
     
     provides org.slf4j.spi.SLF4JServiceProvider with ch.qos.logback.classic.spi.LogbackServiceProvider;
 
