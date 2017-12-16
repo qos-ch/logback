618d14a1242b6077cc5aab170692383606f88c37
Ceki Gulcu <ceki@qos.ch>
2017-12-16 11:59:53 +0100

    working versions of module-info.java

--- logback-classic/src/main/java/module-info.java
+++ logback-classic/src/main/java/module-info.java
@@ -1,12 +1,9 @@
 module ch.qos.logback.classic { 
-
-    requires org.slf4j;
-    requires static java.logging;
-    requires static java.management;
-    
-    requires ch.qos.logback.core;
-    requires static groovy;
-    
-    provides org.slf4j.spi.SLF4JServiceProvider with ch.qos.logback.classic.spi.LogbackServiceProvider;
-
+  requires org.slf4j;
+  requires ch.qos.logback.core;
+  requires static groovy;
+  requires static java.management;
+  
+  provides org.slf4j.spi.SLF4JServiceProvider with ch.qos.logback.classic.provider.LogbackServiceProvider;
 }
+
--- logback-core/src/main/java/module-info.java
+++ logback-core/src/main/java/module-info.java
@@ -1,55 +1,39 @@
 module ch.qos.logback.core { 
-    requires transitive java.naming;
-    requires transitive java.xml;
-    
-    requires transitive static java.sql;
+    requires static transitive java.sql;
+    requires static transitive java.naming;
     
     requires static mail;
-    
-    requires static janino;
     requires static transitive javax.servlet.api;
-    
+    requires static janino;
     requires static commons.compiler;
     
-    // exports below this line
+    
     exports ch.qos.logback.core;
-    exports ch.qos.logback.core.status;
     exports ch.qos.logback.core.spi;
-    exports ch.qos.logback.core.helpers;
+    exports ch.qos.logback.core.status;
     
-    exports ch.qos.logback.core.boolex;
-
-    exports ch.qos.logback.core.util;
-    exports ch.qos.logback.core.filter;
-
-    exports ch.qos.logback.core.html;
-
-    exports ch.qos.logback.core.encoder;
-    exports ch.qos.logback.core.sift;
-    
-    exports ch.qos.logback.core.joran;
-    exports ch.qos.logback.core.joran.spi;
-    exports ch.qos.logback.core.joran.util;
     exports ch.qos.logback.core.joran.action;
+    exports ch.qos.logback.core.joran.spi;
+    exports ch.qos.logback.core.joran.event;  
+    exports ch.qos.logback.core.joran.util;
     exports ch.qos.logback.core.joran.conditional;
-    exports ch.qos.logback.core.joran.event;
     
-
-    exports ch.qos.logback.core.pattern;
-    exports ch.qos.logback.core.pattern.parser;
-    exports ch.qos.logback.core.pattern.color;
+    exports ch.qos.logback.core.filter;
     
     exports ch.qos.logback.core.net;
-    exports ch.qos.logback.core.net.ssl;
     exports ch.qos.logback.core.net.server;
-    
-    exports ch.qos.logback.core.rolling;
-    exports ch.qos.logback.core.rolling.helper;    
+    exports ch.qos.logback.core.net.ssl;
 
+    exports ch.qos.logback.core.sift;
+    exports ch.qos.logback.core.util;
+
+    exports ch.qos.logback.core.pattern;
+    exports ch.qos.logback.core.pattern.color;
+ 
+    exports ch.qos.logback.core.html;
     exports ch.qos.logback.core.db;
-    exports ch.qos.logback.core.read;
-    
-    exports ch.qos.logback.core.recovery;
+    exports ch.qos.logback.core.boolex;
     
     
 }
+
