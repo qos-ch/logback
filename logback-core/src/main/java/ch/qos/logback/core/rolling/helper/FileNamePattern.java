f264607fb450081b0ba1045de15ae3307943e91b
Ceki Gulcu <ceki@qos.ch>
2018-01-27 23:31:00 +0100

    fix LOGBACK-1175

--- logback-core/src/main/java/ch/qos/logback/core/rolling/helper/FileNamePattern.java
+++ logback-core/src/main/java/ch/qos/logback/core/rolling/helper/FileNamePattern.java
@@ -199,7 +199,7 @@ public class FileNamePattern extends ContextAwareBase {
             if (p instanceof LiteralConverter) {
                 buf.append(p.convert(null));
             } else if (p instanceof IntegerTokenConverter) {
-                buf.append("(\\d{1,3})");
+                buf.append("(\\d+)");
             } else if (p instanceof DateTokenConverter) {
                 buf.append(p.convert(date));
             }
@@ -218,7 +218,7 @@ public class FileNamePattern extends ContextAwareBase {
             if (p instanceof LiteralConverter) {
                 buf.append(p.convert(null));
             } else if (p instanceof IntegerTokenConverter) {
-                buf.append("\\d{1,2}");
+                buf.append("\\d+");
             } else if (p instanceof DateTokenConverter) {
                 DateTokenConverter<Object> dtc = (DateTokenConverter<Object>) p;
                 buf.append(dtc.toRegex());
