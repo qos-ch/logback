be00d6e80d96c8bd61c68279511e59e61958bc94
Ceki Gulcu <ceki@qos.ch>
2018-02-10 08:25:58 +0100

    allow user code to invoke logback-classic directly

--- logback-classic/src/main/java/module-info.java
+++ logback-classic/src/main/java/module-info.java
@@ -8,6 +8,30 @@ module ch.qos.logback.classic {
   
   
   exports ch.qos.logback.classic;
+  exports ch.qos.logback.classic.boolex;
+  exports ch.qos.logback.classic.db;
+  exports ch.qos.logback.classic.db.names;
+  exports ch.qos.logback.classic.encoder;
+  exports ch.qos.logback.classic.filter;
+  exports ch.qos.logback.classic.gaffer;
+  exports ch.qos.logback.classic.helpers;
+  exports ch.qos.logback.classic.html;
+  exports ch.qos.logback.classic.jmx;
+  exports ch.qos.logback.classic.joran;
+  exports ch.qos.logback.classic.joran.action;
+  exports ch.qos.logback.classic.jul;
+  exports ch.qos.logback.classic.layout;
+  exports ch.qos.logback.classic.log4j;
+  exports ch.qos.logback.classic.net;
+  exports ch.qos.logback.classic.net.server;
+  exports ch.qos.logback.classic.pattern;
+  exports ch.qos.logback.classic.pattern.color;
+  exports ch.qos.logback.classic.selector;
+  exports ch.qos.logback.classic.selector.servlet;
+  exports ch.qos.logback.classic.servlet;
+  exports ch.qos.logback.classic.sift;
   exports ch.qos.logback.classic.spi;
+  exports ch.qos.logback.classic.turbo;
+  exports ch.qos.logback.classic.util;
 }
 
