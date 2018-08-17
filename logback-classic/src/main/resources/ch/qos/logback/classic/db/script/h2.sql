7a450f5dfe3c07f3d70efbabe33d5d19c01183f6
kalgon <kalgon@hotmail.com>
2018-08-17 14:58:38 +0200

    Invalid comments in H2 sql script

--- logback-classic/src/main/resources/ch/qos/logback/classic/db/script/h2.sql
+++ logback-classic/src/main/resources/ch/qos/logback/classic/db/script/h2.sql
@@ -1,12 +1,12 @@
-# Logback: the reliable, generic, fast and flexible logging framework.
-# Copyright (C) 1999-2010, QOS.ch. All rights reserved.
-#
-# See http://logback.qos.ch/license.html for the applicable licensing 
-# conditions.
+-- Logback: the reliable, generic, fast and flexible logging framework.
+-- Copyright (C) 1999-2010, QOS.ch. All rights reserved.
+--
+-- See http://logback.qos.ch/license.html for the applicable licensing 
+-- conditions.
 
-# This SQL script creates the required tables by ch.qos.logback.classic.db.DBAppender.
-#
-# It is intended for H2 databases. 
+-- This SQL script creates the required tables by ch.qos.logback.classic.db.DBAppender.
+--
+-- It is intended for H2 databases. 
 
 
 DROP TABLE logging_event_exception IF EXISTS;
