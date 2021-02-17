1e62f444261457ccc5490f9306a23dfdd9361e6f
Ceki Gulcu <ceki@qos.ch>
2021-02-17 11:28:17 +0100

    remove unused import

--- logback-core/src/main/java/ch/qos/logback/core/model/Model.java
+++ logback-core/src/main/java/ch/qos/logback/core/model/Model.java
@@ -3,8 +3,6 @@ package ch.qos.logback.core.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import ch.qos.logback.core.spi.ContextAwareBase;
-
 /**
  * Abstract representation of configuration elements
  * 
