f728b475bd3076e220471a66c2aebd1a661e0f9d
Ceki Gulcu <ceki@qos.ch>
2021-07-27 22:41:13 +0200

    remove redundant impls
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- logback-core/src/main/java/ch/qos/logback/core/model/AppenderRefModel.java
+++ logback-core/src/main/java/ch/qos/logback/core/model/AppenderRefModel.java
@@ -3,20 +3,6 @@ package ch.qos.logback.core.model;
 public class AppenderRefModel extends Model {
 
 	String ref;
-
-	@Override
-    public boolean isUnhandled() {
-    	return super.isUnhandled();
-	}
-	@Override
-    public boolean isHandled() {
-		return super.isHandled();
-	}   
-	
-	@Override 
-	public void markAsHandled() {
-		super.markAsHandled();
-	}
 	
 	public String getRef() {
 		return ref;
