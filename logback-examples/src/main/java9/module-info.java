a882aa2c973afce9cbab15676964d4fa096a0829
Ceki Gulcu <ceki@qos.ch>
2021-07-30 23:15:11 +0200

    use multiReleaseOutput in compile-plugin, add module-info.java for logback-examples
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

---
+++ logback-examples/src/main/java9/module-info.java
@@ -0,0 +1,10 @@
+module logback.examples {
+    requires org.slf4j;
+    requires ch.qos.logback.core;
+    requires ch.qos.logback.classic;    
+    
+    
+    //    exports chapters.configuration;
+    
+}
+
