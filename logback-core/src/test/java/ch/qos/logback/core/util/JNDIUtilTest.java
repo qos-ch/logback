e5a5136c2e30c11be510d70065cfff67837ac928
conni <cbuschka@gmail.com>
2021-12-20 20:18:13 +0100

    improve test coverage on JNDIUtil
    Signed-off-by: conni <cbuschka@gmail.com>

--- logback-core/src/test/java/ch/qos/logback/core/util/JNDIUtilTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/util/JNDIUtilTest.java
@@ -1,13 +1,55 @@
 package ch.qos.logback.core.util;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import javax.naming.Context;
 import javax.naming.NamingException;
 
+import ch.qos.logback.core.CoreConstants;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 public class JNDIUtilTest {
+
+	private static final String ALLOWED_JNDI_NAME = "java:...";
+
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+	@Mock
+	private Object mockObject;
+	@Mock
+	private Context mockContext;
+
+	@Test
+	public void shouldNotFailIfContextNull() throws NamingException {
+		Object result = JNDIUtil.lookup(null, ALLOWED_JNDI_NAME);
+
+		assertNull(result);
+	}
+
+	@Test
+	public void shouldNotFailIfNameNull() throws NamingException {
+		Object result = JNDIUtil.lookup(mockContext, null);
+
+		assertNull(result);
+	}
+
+	@Test
+	public void allowJavaNameSpace() throws NamingException {
+		when(mockContext.lookup(ALLOWED_JNDI_NAME)).thenReturn(mockObject);
+		when(mockObject.toString()).thenReturn("toStringResult");
+
+		Object result = JNDIUtil.lookup(mockContext, ALLOWED_JNDI_NAME);
+
+		assertEquals("toStringResult", result);
+	}
 
 	@Test
 	public void ensureJavaNameSpace() throws NamingException {
@@ -16,15 +58,15 @@ public class JNDIUtilTest {
 		try {
 			JNDIUtil.lookup(ctxt, "ldap:...");
 		} catch (NamingException e) {
-			String excaptionMsg = e.getMessage();
-			if(excaptionMsg.startsWith(JNDIUtil.RESTRICTION_MSG)) 
+			String exceptionMsg = e.getMessage();
+			if(exceptionMsg.startsWith(JNDIUtil.RESTRICTION_MSG))
 				return;
 			else {
 				fail("unexpected exception " + e);
 			}
 		}
 
-		fail("Should aNot yet implemented");
+		fail("Should fail if JNDI namespace is not java");
 	}
 
 }
