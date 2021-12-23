119de04a8c7423373f43b334f2544c1ab575887c
Ceki Gulcu <ceki@qos.ch>
2021-12-23 20:14:15 +0100

    site improvemetns
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- logback-site/src/site/pages/templates/header.js
+++ logback-site/src/site/pages/templates/header.js
@@ -1,12 +1,24 @@
-document.write('<div id="header"');
-document.write('  <table width="100%" border="0"><tr>');
-document.write('  <td><a href="http://logback.qos.ch/">');
+document.write('<div id="header">');
+document.write('  <table width="100%" border="0">');
+document.write(' <tr>');
+document.write('  <td><a href="https://logback.qos.ch/">');
 document.write('  <img src="' + prefix + 'images/logos/lblogo.jpg" alt="" border="0"/>');
 document.write('  </a></td>')
 
-document.write('  <td>&nbsp;</td>');
 
-document.write('  </tr></table>')
+//document.write('  <td>&nbsp;</td>');
+
+document.write(' <td style="vertical-align:bottom;">');
+document.write('    <div style="display: flex; float: right; align-items: flex-end;">');
+document.write('      <a href="https://github.com/qos-ch/logback" style="xmargin-left: auto;">');
+document.write('        <img src="' + prefix + 'images/GitHub-Mark-32px.png" alt="Source code" border="0"/>');
+document.write('      </a>');
+document.write('      <a href="https://twitter.com/qos_ch" style="margin-left: 1ex">');
+document.write('        <img height="32px" src="' + prefix + 'images/TwitterLogo_blue.svg" alt="Follow @qos_ch on Twitter" border="0"/>');
+document.write('       </a>');
+document.write('    </div>')
+document.write('   </td>');
+document.write(' </tr></table>')
 
 
 document.write('  <div id="headerLine"></div>');
