8b618b8bf91e8aff69d5a2c26f030bfb65542953
Ceki Gulcu <ceki@qos.ch>
2021-12-24 11:35:14 +0100

    site updates
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- logback-site/src/site/pages/templates/header.js
+++ logback-site/src/site/pages/templates/header.js
@@ -13,7 +13,7 @@ document.write('    <div style="display: flex; float: right; align-items: flex-e
 document.write('      <a href="https://github.com/qos-ch/logback" style="xmargin-left: auto;">');
 document.write('        <img src="' + prefix + 'images/GitHub-Mark-32px.png" alt="Source code" border="0"/>');
 document.write('      </a>');
-document.write('      <a href="https://twitter.com/qos_ch" style="margin-left: 1ex">');
+document.write('      <a href="https://twitter.com/qos_ch" style="margin-left: 1ex; margin-right: 16em;">');
 document.write('        <img height="32px" src="' + prefix + 'images/TwitterLogo_blue.svg" alt="Follow @qos_ch on Twitter" border="0"/>');
 document.write('       </a>');
 document.write('    </div>')
