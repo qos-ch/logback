625735bc2301b1e2e0ac8685d08b3db6976d868b
Ceki Gulcu <ceki@qos.ch>
2022-01-26 20:49:53 +0100

    add sponsorship request to header
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- logback-site/src/site/pages/templates/header.js
+++ logback-site/src/site/pages/templates/header.js
@@ -6,23 +6,32 @@ document.write('       <img src="' + prefix + 'images/logos/lblogo.jpg" alt="" b
 document.write('      </a>')
 document.write('  </td>')
 
+document.write('   <td style="vertical-align:bottom;">')
+document.write('     <p style="background-color: #fffbd9; border-color: #db4545; border-width: thin;')
+document.write('        border-style: dotted; padding: 5px; margin-left: 6ex;">');
+document.write('     Sponsor us for the purchase of dedicated hardware. This will enable the production of automated and <b>isolated</b> releases.</p>');
+document.write('   </td>');
+
+
 //document.write('  <td>&nbsp;</td>');
 
 document.write(' <td style="vertical-align:bottom;">');
-document.write('    <div style="display: flex; float: right; align-items: flex-end;">');
+document.write('   <div style="display: flex; float: right; align-items: flex-end;">');
 
-
-
-document.write('      <a href="https://github.com/qos-ch/logback" style="xmargin-left: auto;">');
-document.write('        <img src="' + prefix + 'images/GitHub-Mark-32px.png" alt="Source code" border="0"/>');
-document.write('      </a>');
 document.write('       <iframe src="https://github.com/sponsors/qos-ch/button" title="Sponsor qos-ch" height="35" width="116" style="border: 0; margin-left: 1ex;"></iframe>');
+
+
+document.write('    <a href="https://github.com/qos-ch/logback" style="xmargin-left: 1ex;">');
+document.write('       <img src="' + prefix + 'images/GitHub-Mark-32px.png" alt="Source code" border="0"/>');
+document.write('    </a>');
+
 
 document.write('      <a href="https://twitter.com/qos_ch" style="margin-left: 1ex; margin-right: 16em;">');
 document.write('        <img height="32px" src="' + prefix + 'images/TwitterLogo_blue.svg" alt="Follow @qos_ch on Twitter" border="0"/>');
 document.write('       </a>');
 document.write('    </div>')
 document.write('   </td>');
+
 document.write(' </tr></table>')
 
 
