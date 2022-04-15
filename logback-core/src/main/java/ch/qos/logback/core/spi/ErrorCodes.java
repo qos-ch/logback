1f20887d3382256fb1c426686624c9fa48f8e28a
Ceki Gulcu <ceki@qos.ch>
2022-04-15 15:02:17 +0200

    fix LOGBACK-1628
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

---
+++ logback-core/src/main/java/ch/qos/logback/core/spi/ErrorCodes.java
@@ -0,0 +1,23 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+package ch.qos.logback.core.spi;
+
+public class ErrorCodes {
+
+    
+    public static final String EMPTY_MODEL_STACK = "Could not find valid configuration instuctions. Exiting.";
+    public static final String PARENT_MODEL_NOT_FOUND = "Could not find parent model.";
+    public static final String SKIPPING_IMCICIT_MODEL_ADDITION = " Will not add current implicit model as subModel.";
+    
+}
