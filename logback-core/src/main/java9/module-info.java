1e384878cef7f9fe7009467b203dac07b991ad60
Ceki Gulcu <ceki@qos.ch>
2022-09-30 16:51:40 +0200

    revert to janino publicly available version
    cherry-pick 5f25ff98636abd8e78b1ee747e34b5c3f6c59ae9
    
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- logback-core/src/main/java9/module-info.java
+++ logback-core/src/main/java9/module-info.java
@@ -8,12 +8,13 @@ module ch.qos.logback.core {
     requires static transitive jakarta.mail;
     //requires static jakarta.activation;
     
-    requires static jakarta.servlet;
+    requires static transitive java.servlet;
+
     requires static janino;
     requires static commons.compiler;
 
-    requires static org.codehaus.janino.janino;
-    requires static org.codehaus.janino.commons.compiler;
+    //requires static org.codehaus.janino.janino;
+    //requires static org.codehaus.janino.commons.compiler;
 
     exports ch.qos.logback.core;
     exports ch.qos.logback.core.boolex;
