55502546142897ef6d579568ac8ec8933577e88a
Ceki Gulcu <ceki@qos.ch>
2022-11-29 23:08:54 +0100

    Status takes into account timestamp in equality
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- logback-core/src/test/java/ch/qos/logback/core/BasicStatusManagerTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/BasicStatusManagerTest.java
@@ -50,20 +50,22 @@ public class BasicStatusManagerTest {
     public void many() {
         int margin = 300;
         int len = MAX_HEADER_COUNT + TAIL_SIZE + margin;
+        List<Status> witness = new ArrayList<Status>();
         for (int i = 0; i < len; i++) {
-            bsm.add(new ErrorStatus("" + i, this));
+            Status s = new ErrorStatus("" + i, this);
+            bsm.add(s);
+            if(i < MAX_HEADER_COUNT) {
+                witness.add(new ErrorStatus("" + i, this));
+            }
+            if(i >= MAX_HEADER_COUNT + margin) {
+                witness.add(s);
+            }
         }
 
         List<Status> statusList = bsm.getCopyOfStatusList();
         assertNotNull(statusList);
         assertEquals(MAX_HEADER_COUNT + TAIL_SIZE, statusList.size());
-        List<Status> witness = new ArrayList<Status>();
-        for (int i = 0; i < MAX_HEADER_COUNT; i++) {
-            witness.add(new ErrorStatus("" + i, this));
-        }
-        for (int i = 0; i < TAIL_SIZE; i++) {
-            witness.add(new ErrorStatus("" + (MAX_HEADER_COUNT + margin + i), this));
-        }
+
         assertEquals(witness, statusList);
     }
 
