aebfbf4c5ad8bf63cbd4d57282b047a06bd125f0
Henry Kupty <hkupty@gmail.com>
2023-04-04 22:33:59 +0200

    feat: Add JsonEncoder for logback-classic
    Signed-off-by: Henry Kupty <hkupty@gmail.com>

--- logback-core/src/main/java/ch/qos/logback/core/encoder/JsonEncoder.java
+++ logback-core/src/main/java/ch/qos/logback/core/encoder/JsonEncoder.java
@@ -1,14 +1,28 @@
 package ch.qos.logback.core.encoder;
 
-public class JsonEncoder<E> extends EncoderBase<E> {
-    @Override
-    public byte[] headerBytes() {
-        return new byte[0];
+import ch.qos.logback.core.util.DirectJson;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * This class allows for concrete encoders to write json log messages.
+ *
+ * @param <E>
+ * @author Henry John Kupty
+ */
+public abstract class JsonEncoder<E> extends EncoderBase<E> {
+    @FunctionalInterface
+    protected interface Emitter<E> {
+        void write(E event);
     }
 
+    private static final byte[] LINE_BREAK = System.getProperty("line.separator").getBytes(StandardCharsets.UTF_8);
+
+    protected DirectJson jsonWriter = new DirectJson();
+
     @Override
-    public byte[] encode(E event) {
-        return new byte[0];
+    public byte[] headerBytes() {
+        return LINE_BREAK;
     }
 
     @Override
