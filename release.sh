a8bf4ecee70859390c22b316c01d5e8350a3cd79
Besmir Beqiri <besmir.beqiri@logicbind.io>
2023-05-11 20:07:35 +0200

    Prepare the release for version `1.4.7-jpro`

--- release.sh
+++ release.sh
@@ -1,25 +1,25 @@
-mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${VERSION_NUMBER} 
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=1.4.7-jpro
 
 mvn clean
-mvn install
+mvn install -DskipTests=true
 mvn animal-sniffer:check
-mvn site:site
+#mvn site:site
 
 #mvn javadoc:jar
 #mvn assembly:single
 
-export GPG_TTY=$(tty)
-password
-mvn deploy -P javadocjar,sign-artifacts -Dgpg.passphrase=passwd
-
+#export GPG_TTY=$(tty)
+#password
+#mvn deploy -P javadocjar,sign-artifacts -Dgpg.passphrase=passwd
+mvn deploy -DskipTests=true -P javadocjar
 
 
 # cleanHistory dep
 #uncomment diffie-hellman support in /etc/ssh/sshd_config
 
-mvn site:deploy -N # with Java 8!!!
+#mvn site:deploy -N # with Java 8!!!
 
-git tag -m "tagging" -a v_${VERSION_NUMBER}
-git push --tags
+#git tag -m "tagging" -a v_${VERSION_NUMBER}
+#git push --tags
 
-release version and add next version on jira
+#release version and add next version on jira
