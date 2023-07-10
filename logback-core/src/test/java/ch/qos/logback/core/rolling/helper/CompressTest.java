dc16151ca9993a5b22b34b40f5801d3b8d28535f
Alfusainey Jallow <alf.jallow@gmail.com>
2023-07-10 09:36:28 +0200

    [LOGBACK-1755] Close streams
    Signed-off-by: Alfusainey Jallow <alf.jallow@gmail.com>

--- logback-core/src/test/java/ch/qos/logback/core/rolling/helper/CompressTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/rolling/helper/CompressTest.java
@@ -111,15 +111,14 @@ public class CompressTest {
     }
 
     private void copy(File src, File dst) throws IOException {
-        InputStream in = new FileInputStream(src);
-        OutputStream out = new FileOutputStream(dst);
-        byte[] buf = new byte[1024];
-        int len;
-        while ((len = in.read(buf)) > 0) {
-            out.write(buf, 0, len);
+        try (InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dst);) {
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
         }
-        in.close();
-        out.close();
     }
 
 }
