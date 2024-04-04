7587630663c892b56edf613e8b63178ab8ac4f5d
Ceki Gulcu <ceki@qos.ch>
2024-04-04 22:25:02 +0200

    more complete initialization
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- logback-classic/src/test/java/ch/qos/logback/classic/issue/logback_1759/Logback1759Test.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/issue/logback_1759/Logback1759Test.java
@@ -12,27 +12,45 @@
  * as published by the Free Software Foundation.
  */
 
-package ch.qos.logback.core.issue.logback_1759;
+package ch.qos.logback.classic.issue.logback_1759;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
 import ch.qos.logback.core.ConsoleAppender;
-import ch.qos.logback.core.Context;
-import ch.qos.logback.core.ContextBase;
-import ch.qos.logback.core.encoder.EchoEncoder;
+import ch.qos.logback.core.util.StatusPrinter2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.spi.MDCAdapter;
 
 public class Logback1759Test {
 
-    Context context = new ContextBase();
-    EchoEncoder echoEncoder = new EchoEncoder();
-    ConsoleAppender consoleAppender = new ConsoleAppender();
+    LoggerContext context = new LoggerContext();
+    Logger logger = context.getLogger("toto.foo");
+    StatusPrinter2 statusPrinter2 = new StatusPrinter2();
+    PatternLayoutEncoder patternLayoutEncoder = null;
+    ConsoleAppender consoleAppender = null;
+    LogbackMDCAdapter logbackMDCAdapter = new LogbackMDCAdapter();
 
     @BeforeEach
     public void setup() {
+        context.setMDCAdapter(logbackMDCAdapter);
+        init();
+    }
+
+    void init() {
+        System.out.println("Init called");
+        this.patternLayoutEncoder = new PatternLayoutEncoder();
+        patternLayoutEncoder.setContext(context);
+        patternLayoutEncoder.setPattern("%highlight(%level) %message%n");
+        patternLayoutEncoder.start();
+
+        this.consoleAppender = new ConsoleAppender();
         consoleAppender.setContext(context);
-        echoEncoder.setContext(context);
-        echoEncoder.start();
-        consoleAppender.setEncoder(echoEncoder);
+        consoleAppender.setEncoder(patternLayoutEncoder);
     }
 
     @Test
@@ -40,10 +58,20 @@ public class Logback1759Test {
         consoleAppender.setWithJansi(true);
         consoleAppender.start();
 
-        consoleAppender.doAppend("hello 1");
+        //String fqcn, Logger logger, Level level, String message, Throwable throwable,
+        //        Object[] argArray
+
+        LoggingEvent le = new LoggingEvent("x", logger, Level.INFO, "hello", null, null);
+
+        consoleAppender.doAppend(le);
 
         consoleAppender.stop();
+
+        init();
         consoleAppender.start();
-        consoleAppender.doAppend("hello 2");
+
+        consoleAppender.doAppend(le);
+
+        //statusPrinter2.print(context);
     }
 }
