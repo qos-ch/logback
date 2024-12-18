0056a9cb9fc950df348aa0e129b09e27e28945ad
Ceki Gulcu <ceki@qos.ch>
2024-12-18 16:08:57 +0100

    CORE_POOL_SIZE set to 4
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- logback-core/src/main/java/ch/qos/logback/core/CoreConstants.java
+++ logback-core/src/main/java/ch/qos/logback/core/CoreConstants.java
@@ -27,16 +27,16 @@ public class CoreConstants {
     /**
      * Number of idle threads to retain in a context's executor service.
      */
-    public static final int CORE_POOL_SIZE = 0;
+    public static final int CORE_POOL_SIZE = 4;
 
     // In Java 21 and later the actual threads are assumed to be virtual
     public static final int SCHEDULED_EXECUTOR_POOL_SIZE = 4;
 
     /**
      * Maximum number of threads to allow in a context's executor service.
+     * @deprecated no longer used
+     *
      */
-    // if you need a different MAX_POOL_SIZE, please file create a github issue
-    // asking for a larger MAX_POOL_SIZE parameter.
     public static final int MAX_POOL_SIZE = 32;
 
     // Note that the line.separator property can be looked up even by
