e369c21bf64e6d39183d8de2bcea35e85517782a
Mark Chesney <mches@users.noreply.github.com>
2024-12-20 19:29:51 -0700

    Fix thread safety, clean up Javadoc
    Signed-off-by: Mark Chesney <mches@users.noreply.github.com>

--- logback-core/src/test/java/ch/qos/logback/core/net/mock/MockContext.java
+++ logback-core/src/test/java/ch/qos/logback/core/net/mock/MockContext.java
@@ -1,6 +1,6 @@
 /**
  * Logback: the reliable, generic, fast and flexible logging framework.
- * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
+ * Copyright (C) 1999-2024, QOS.ch. All rights reserved.
  *
  * This program and the accompanying materials are dual-licensed under
  * either the terms of the Eclipse Public License v1.0 as published by
@@ -15,8 +15,6 @@ package ch.qos.logback.core.net.mock;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
 
 import ch.qos.logback.core.Context;
 import ch.qos.logback.core.ContextBase;
@@ -53,36 +51,44 @@ public class MockContext extends ContextBase {
         return lastStatus;
     }
 
-    public void setLastStatus(Status lastStatus) {
-        this.lastStatus = lastStatus;
-    }
-
     private class MockStatusManager implements StatusManager {
 
+        @Override
         public void add(Status status) {
             lastStatus = status;
         }
 
+        @Override
         public List<Status> getCopyOfStatusList() {
             throw new UnsupportedOperationException();
         }
 
+        @Override
+        public int getLevel() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
         public int getCount() {
             throw new UnsupportedOperationException();
         }
 
+        @Override
         public boolean add(StatusListener listener) {
             throw new UnsupportedOperationException();
         }
 
+        @Override
         public void remove(StatusListener listener) {
             throw new UnsupportedOperationException();
         }
 
+        @Override
         public void clear() {
             throw new UnsupportedOperationException();
         }
 
+        @Override
         public List<StatusListener> getCopyOfStatusListenerList() {
             throw new UnsupportedOperationException();
         }
