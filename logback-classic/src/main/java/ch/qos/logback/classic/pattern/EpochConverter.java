b65040a3b5d844a791bd3cc690ca44e9e024e04d
Duncan Jauncey <duncan.jauncey@gmail.com>
2026-01-16 10:01:52 +0000

    Add EpochConverter for milliseconds/seconds since epoch (related to issue #968) (#1000)
    * Add EpochConverter and pattern "ep", "epoch" (related to issue #968)
    
    Pattern "ep" or "epoch" supports milliseconds by default and also epoch{seconds} for people who need UNIX-style epoch timestamps in their logs.
    
    Signed-off-by: Duncan Jauncey <duncan@duncanjauncey.com>
    
    * Add copyright notice to EpochConverter and EpochConverterTest
    
    Signed-off-by: Duncan Jauncey <duncan@duncanjauncey.com>
    
    ---------
    
    Signed-off-by: Duncan Jauncey <duncan@duncanjauncey.com>
    Co-authored-by: Duncan <duncan@duncanjauncey.com>

---
+++ logback-classic/src/main/java/ch/qos/logback/classic/pattern/EpochConverter.java
@@ -0,0 +1,28 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+package ch.qos.logback.classic.pattern;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+public class EpochConverter extends ClassicConverter {
+    @Override
+    public String convert(ILoggingEvent event) {
+        String millisOrSeconds = getFirstOption();
+        if ("seconds".equals(millisOrSeconds) ) {
+            return "" + (event.getTimeStamp() / 1000L);
+        }
+
+        return "" + event.getTimeStamp();
+    }
+}
