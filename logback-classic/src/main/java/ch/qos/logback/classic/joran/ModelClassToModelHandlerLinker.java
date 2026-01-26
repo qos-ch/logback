145a2279a0d97865905d8a34fdfdf7734282571b
ceki <ceki@qos.ch>
2026-01-26 10:40:08 +0100

    update license to EPL 2.0 in logback-classic
    Signed-off-by: ceki <ceki@qos.ch>

--- logback-classic/src/main/java/ch/qos/logback/classic/joran/ModelClassToModelHandlerLinker.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/joran/ModelClassToModelHandlerLinker.java
@@ -1,9 +1,9 @@
 /*
  * Logback: the reliable, generic, fast and flexible logging framework.
- * Copyright (C) 1999-2023, QOS.ch. All rights reserved.
+ * Copyright (C) 1999-2026, QOS.ch. All rights reserved.
  *
  * This program and the accompanying materials are dual-licensed under
- * either the terms of the Eclipse Public License v1.0 as published by
+ * either the terms of the Eclipse Public License v2.0 as published by
  * the Eclipse Foundation
  *
  *   or (per the licensee's choosing)
